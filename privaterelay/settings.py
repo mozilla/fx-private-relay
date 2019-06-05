2054296a4a2925313d7ed4ca2ca9969406a03c61
groovecoder <luke.crouch@gmail.com>
2019-06-05 04:55:05 -0500

    fix django_heroku

--- privaterelay/settings.py
+++ privaterelay/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
