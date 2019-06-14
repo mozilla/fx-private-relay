51ceb8657ec4d352fc6b60a617919b212be0fd20
groovecoder <luke.crouch@gmail.com>
2019-06-14 16:27:33 -0500

    fix email models string display

--- emails/admin.py
+++ emails/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import RelayAddress, Message
+from .models import RelayAddress, Message, Profile
 
 
 class RelayAddressAdmin(admin.ModelAdmin):
@@ -11,5 +11,10 @@ class MessageAdmin(admin.ModelAdmin):
     pass
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(RelayAddress, RelayAddressAdmin)
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Profile, ProfileAdmin)
--- emails/models.py
+++ emails/models.py
@@ -10,6 +10,9 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     api_token = models.UUIDField(default=uuid.uuid4)
 
+    def __str__(self):
+        return '%s Profile' % self.user
+
 
 def address_default():
     return ''.join(random.choices(string.ascii_lowercase + string.digits, k=9))
@@ -21,9 +24,17 @@ class RelayAddress(models.Model):
         max_length=64, default=address_default, unique=True
     )
 
+    def __str__(self):
+        return self.address
+
 
 class Message(models.Model):
     relay_address = models.ForeignKey(RelayAddress, on_delete=models.CASCADE)
     from_address = models.CharField(max_length=255)
     subject = models.CharField(max_length=255)
     message = models.TextField()
+
+    def __str__(self):
+        return '%s, From: %s, To: %s' % (
+            self.subject, self.from_address, self.relay_address
+        )
