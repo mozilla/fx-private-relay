6a2512e2dc456541f4240f90c7871fb482942662
groovecoder <luke.crouch@gmail.com>
2020-03-09 21:42:51 -0500

    fix EmailAddress constructor args

--- emails/views.py
+++ emails/views.py
@@ -103,8 +103,9 @@ def _inbound_logic(json_body):
     sl_message.subject = subject
     sl_message.html_body = html
     sl_message.plain_text_body = text
+    relay_from_address, relay_from_display = _generate_relay_From(from_address)
     sl_message.from_email_address = EmailAddress(
-        _generate_relay_From(from_address)
+        relay_from_address, relay_from_display
     )
     sl_message.to_email_address.append(EmailAddress(relay_address.user.email))
     socketlabs_client = SocketLabsClient(
@@ -118,6 +119,6 @@ def _generate_relay_From(original_from_address):
     relay_display_name, relay_from_address = parseaddr(
         settings.RELAY_FROM_ADDRESS
     )
-    return '%s via Firefox Private Relay <%s>' % (
-        original_from_address, relay_from_address
+    return relay_from_address, '%s via Firefox Private Relay' % (
+        original_from_address
     )
