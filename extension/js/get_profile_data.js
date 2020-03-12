a80dab8ad6d7f1eb8b2d1f4219640c816b03421d
groovecoder <luke.crouch@gmail.com>
2020-03-12 11:03:15 -0500

    update add-on to get api key from profile

---
+++ extension/js/get_profile_data.js
@@ -0,0 +1,19 @@
+(async function () {
+  // Get the api token from the account profile page
+  const profileMainElement = document.querySelector("#profile-main");
+  const apiToken = profileMainElement.dataset.apiToken;
+  chrome.storage.local.set({apiToken}, () => {
+    console.log("Set the API token.");
+  });
+
+  // Get and store the relay addresses from the account profile page,
+  // so they can be used later, even if the API endpoint is down
+  const relayAddressElements = document.querySelectorAll(".relay-address");
+  const relayAddresses = [];
+  for (const relayAddressEl of relayAddressElements) {
+    relayAddresses.push(relayAddressEl.dataset.clipboardText);
+  }
+  chrome.storage.local.set({relayAddresses}, () => {
+    console.log("Set the relayAddresses.");
+  });
+})();
