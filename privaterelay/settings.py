76aab64e366fc465c6368b1a406acfd401b28f30
groovecoder <luke.crouch@gmail.com>
2020-03-11 21:47:46 -0500

    'Add to Firefox' button to download .zip from media/

--- privaterelay/settings.py
+++ privaterelay/settings.py
@@ -181,6 +181,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 SITE_ID = 1
 
 AUTHENTICATION_BACKENDS = (
