7804f9bfcfe71b1caa11800da4847f835cb9bbbb
Andreas Wagner <mail@andreaswagner.org>
2020-04-04 14:38:41 +0200

    Avoid 'executeScript' when filling relay address, use content-script file

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,42 +1,5 @@
 const RELAY_SITE_ORIGIN = "http://127.0.0.1:8000";
 
-/**
- * Escapes any occurances of &, ", <, > or / with XML entities.
- *
- * @param {string} str
- *        The string to escape.
- * @return {string} The escaped string.
- */
-function escapeXML(str) {
-  const replacements = { "&": "&amp;", "\"": "&quot;", "'": "&apos;", "<": "&lt;", ">": "&gt;", "/": "&#x2F;" };
-  return String(str).replace(/[&"'<>/]/g, m => replacements[m]);
-}
-
-/**
- * A tagged template function which escapes any XML metacharacters in
- * interpolated values.
- *
- * @param {Array<string>} strings
- *        An array of literal strings extracted from the templates.
- * @param {Array} values
- *        An array of interpolated values extracted from the template.
- * @returns {string}
- *        The result of the escaped values interpolated with the literal
- *        strings.
- */
-
-function escaped(strings, ...values) {
-  const result = [];
-
-  for (const [i, string] of strings.entries()) {
-    result.push(string);
-    if (i < values.length)
-      result.push(escapeXML(values[i]));
-  }
-
-  return result.join("");
-}
-
 async function makeRelayAddress() {
   const apiToken = await browser.storage.local.get("apiToken");
 
@@ -72,9 +35,10 @@ async function makeRelayAddressForTargetElement(info, tab) {
     return;
   }
 
-  browser.tabs.executeScript(tab.id, {
-    frameId: info.frameId,
-    code: escaped`let inputElement = browser.menus.getTargetElement(${info.targetElementId});inputElement.value = "${newRelayAddressResponse}";`,
+  browser.tabs.sendMessage(tab.id, {
+    type: "fillTargetWithRelayAddress",
+    targetElementId : info.targetElementId,
+    relayAddress: newRelayAddressResponse,
   });
 }
 
