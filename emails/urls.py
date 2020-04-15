fe6161dd928a30d4152b6129b29a3f804a2115a0
groovecoder <luke.crouch@gmail.com>
2020-04-14 20:08:44 -0500

    fix #163: _get_data_from_request view function for JS fetch requests

--- emails/urls.py
+++ emails/urls.py
@@ -6,5 +6,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='emails-index'),
     path('inbound', views.inbound),
-    path('toggle-forwarding', views.toggle_forwarding, name='emails-toggle-forwarding')
 ]
