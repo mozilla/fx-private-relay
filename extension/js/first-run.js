dbe12fdbd40f0fd409cba49bbf1eb13e1ad31cf0
lesley <lnorton@mozilla.com>
2020-07-20 15:42:09 -0500

    Remove modal, nest data preference in PIE page

--- extension/js/first-run.js
+++ extension/js/first-run.js
@@ -7,43 +7,11 @@ document.addEventListener("DOMContentLoaded", async () => {
   const { relaySiteOrigin } = await browser.storage.local.get("relaySiteOrigin");
 
 
-  setTimeout(() => {
-    openModal();
-  }, 500);
-
-  const privacyModal = document.querySelector(".privacy-modal");
-
-  const closeModal = () => {
-    privacyModal.classList.add("hide-modal");
-    document.removeEventListener("keydown", closeModalOnEscape);
-    document.removeEventListener("click", closeModalOnPageClick);
-  };
-
-  const closeModalOnPageClick = (e) => {
-    const evtTarget = e.target;
-    const classListString = Array.from(evtTarget.classList).join();
-    if (!classListString.includes("modal")) {
-      closeModal();
-    }
-  };
-
-  const closeModalOnEscape = (e) => {
-    if (e.key === "Escape") {
-      closeModal();
-    }
-  };
-
   document.addEventListener("focus", () => {
-    if (!privacyModal.classList.contains("hide-modal"));
     enableDataOptOut();
   });
 
-  const openModal = () => {
-    privacyModal.classList.remove("hide-modal");
-    document.addEventListener("keydown", closeModalOnEscape);
-    enableDataOptOut();
-    document.addEventListener("click", closeModalOnPageClick);
-  }
+  enableDataOptOut();
 
   // TODO Add FXA params, do metrics flow from extension?
   const openFxaFlow = new URL(fxaOauthFlow, relaySiteOrigin);
