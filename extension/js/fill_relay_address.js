016db2ca7da05d4b05f52e41a521bc2283346b9f
lesleyjanenorton <lesley@lesleynorton.com>
2020-08-14 10:17:21 -0500

    Fix "Manage All Aliases" link in modal / clip dead modal code

--- extension/js/fill_relay_address.js
+++ extension/js/fill_relay_address.js
@@ -1,4 +1,4 @@
-async function showModal(modalType, newAlias=null) {
+async function showModal(modalType) {
   const { relaySiteOrigin } = await browser.storage.local.get("relaySiteOrigin");
   const modalWrapper = document.createElement("div");
   modalWrapper.classList = ["fx-relay-modal-wrapper"];
@@ -16,57 +16,29 @@ async function showModal(modalType, newAlias=null) {
 
   modalContent.appendChild(logoWrapper);
 
-  const modalAliasWrapper = document.createElement("div");
-  modalAliasWrapper.classList = ["fx-relay-modal-message-alias-wrapper"];
-
-  if (modalType === "new-alias") { // New alias was created, but input wasn't found.
-    const modalAlias = document.createElement("span");
-    modalAlias.textContent = newAlias;
-    modalAlias.classList = ["relay-modal-alias"];
-
-    const purpleCopiedBlurb = document.createElement("span");
-    purpleCopiedBlurb.textContent = "Copied!";
-    purpleCopiedBlurb.classList = ["fx-relay-message-copied"];
-
-    [modalAlias, purpleCopiedBlurb].forEach(textEl => {
-      modalAliasWrapper.appendChild(textEl);
-    });
-
-    const modalMessage = document.createElement("span");
-    modalMessage.textContent = "You just created a new alias!";
-    modalMessage.classList = ["fx-relay-modal-message"];
-
-    [modalAliasWrapper, modalMessage].forEach(textEl => {
-      modalContent.appendChild(textEl);
-    });
-    window.navigator.clipboard.writeText(newAlias);
-  }
-
   const sendModalEvent = (evtAction, evtLabel) => {
     return sendRelayEvent("Modal", evtAction, evtLabel);
   };
 
-  if (modalType === "max-num-aliases") { // User has maxed out the number of allowed free aliases.
+  sendModalEvent("viewed-modal", "modal-max-aliases");
+  const modalMessage = document.createElement("span");
 
-    sendModalEvent("viewed-modal", "modal-max-aliases");
-    const modalMessage = document.createElement("span");
+  modalMessage.textContent = "You've reached the alias limit for our beta release.";
+  modalMessage.classList = ["fx-relay-modal-message"];
+  modalContent.appendChild(modalMessage);
 
-    modalMessage.textContent = "You've used all of your beta aliases.";
-    modalMessage.classList = ["fx-relay-modal-message"];
-    modalContent.appendChild(modalMessage);
+  const manageAliasesLink = document.createElement("a");
+  manageAliasesLink.textContent = "Manage All Aliases";
+  manageAliasesLink.classList = ["fx-relay-new-tab fx-relay-modal-manage-aliases"];
+  manageAliasesLink.href = `${relaySiteOrigin}?utm_source=fx-relay-addon&utm_medium=context-menu-modal&utm_campaign=beta&utm_content=manage-relay-addresses`;
 
-    const manageAliasesLink = document.createElement("a");
-    manageAliasesLink.textContent = "Manage All Aliases";
-    manageAliasesLink.classList = ["fx-relay-new-tab"];
-    manageAliasesLink.href = `${relaySiteOrigin}?utm_source=fx-relay-addon&utm_medium=context-menu-modal&utm_campaign=beta&utm_content=manage-relay-addresses`;
-
-    manageAliasesLink.addEventListener("click", async(e) => {
-      e.preventDefault();
-      sendModalEvent("click", "modal-manage-all-aliases-btn");
-      return await browser.tabs.create({ url: e.target.href });
-    });
-    modalContent.appendChild(manageAliasesLink);
-  }
+  manageAliasesLink.addEventListener("click", async(e) => {
+    e.preventDefault();
+    sendModalEvent("click", "modal-manage-all-aliases-btn");
+    return window.open(e.target.href);
+  });
+  modalContent.appendChild(manageAliasesLink);
+  
 
   const modalCloseButton = document.createElement("button");
   modalCloseButton.classList = ["fx-relay-modal-close-button"];
@@ -104,15 +76,11 @@ function fillInputWithAlias(emailInput, relayAlias) {
 
 browser.runtime.onMessage.addListener((message, sender, response) => {
   if (message.type === "fillTargetWithRelayAddress") {
-    // attempt to find the email input
     const emailInput = browser.menus.getTargetElement(message.targetElementId);
-    if (!emailInput) {
-      return showModal("new-alias", message.relayAddress);
-    }
     return fillInputWithAlias(emailInput, message.relayAddress);
   }
 
   if (message.type === "showMaxNumAliasesMessage") {
-    return showModal("max-num-aliases");
+    return showModal();
   }
 });
