a82c0518258c61e32381bbcc9a1d125bbd4db326
Peter deHaan <peter@deseloper.com>
2020-11-20 11:08:00 -0800

    Move web-ext settings into config file

---
+++ extension/web-ext-config.js
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+ module.exports = {
+  // Global options:
+  sourceDir: ".",
+  artifactsDir: "../static/downloads/addon/latest/",
+  verbose: false,
+
+  // Command options:
+  build: {
+    overwriteDest: true,
+  },
+
+  ignoreFiles: [
+    ".DS_Store",
+    "README.*",
+    "package.json",
+    "package-lock.json",
+    "sequence-diagram.*",
+    "web-ext-config.js"
+  ],
+};
