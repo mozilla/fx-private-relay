90c87a06577148babeca9544538f785d89141c2c
Maxx Crawford <maxx.crawford@gmail.com>
2021-06-11 13:30:49 -0500

    Rewrite Gulpfile to use latest syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp = require('gulp');
+const { src, watch, series, dest } = require('gulp');
 const sass = require('gulp-sass');
 const del = require('del');
 const merge = require('merge-stream');
@@ -7,53 +7,44 @@ const merge = require('merge-stream');
 const buildDir = 'static/scss/libs/';
 const finalDir = 'static/css/';
 
-// Compile all SASS/SCSS into => app.scss
-gulp.task('styles', () => {
-    return gulp.src('static/scss/app.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(finalDir));
-});
-
-// Clean out compiled CSS folder
-gulp.task('clean', () => {
-    return del([
+function clean(cb) {
+    del([
         finalDir
     ]);
-});
+    cb();
+}
 
-// On setup, remove 
-gulp.task('setup', () => {
-    return del([
+function reset(cb) {
+    del([
         finalDir,
         buildDir,
     ]);
-});
+    cb();
+}
 
-function assetsCopy() {
-    return merge([
+function styles(cb) {
+    src('static/scss/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest(finalDir));
+
+    cb();
+}
+
+function assetsCopy(cb) {
+    merge([
         // SASS and LESS go to build dir
-        gulp.src([
+        src([
             '!node_modules/@mozilla-protocol/core/*',
             'node_modules/@mozilla-protocol/core/**/*',
             ])
-            .pipe(gulp.dest(buildDir)),
+            .pipe(dest(buildDir)),
     ]);
+    cb();
 }
 
-const buildTask = gulp.series([
-    'setup',
-    assetsCopy,
-    'styles',
-]);
-
-gulp.task('build', buildTask);
-
-gulp.task('default', async () => {
-    // Build on first run
-    gulp.series(buildTask);
-    
-    // Watch for SCSS changes
-    gulp.watch('static/scss/**/*.scss', (done) => {
-        gulp.series(['clean', 'styles'])(done);
-    });
-});
+exports.default = series(
+    reset, assetsCopy, styles, function() {
+        // You can use a single task
+        watch('static/scss/**/*.scss', { ignoreInitial: false }, series(clean, styles));
+    }
+);
