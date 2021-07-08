dbc1a9d995f11a61dbab2d6837553f34ebe16fd9
Se Yeon Kim <sykim@mozilla.com>
2021-07-08 12:32:46 -0500

    Add tracker numbers to DeletedAddress When alias are deleted store the trackers found and DB migration

--- emails/models.py
+++ emails/models.py
@@ -251,6 +251,8 @@ class DeletedAddress(models.Model):
     num_forwarded = models.PositiveSmallIntegerField(default=0)
     num_blocked = models.PositiveSmallIntegerField(default=0)
     num_spam = models.PositiveSmallIntegerField(default=0)
+    num_open_trackers = models.PositiveSmallIntegerField(default=0)
+    num_click_trackers = models.PositiveSmallIntegerField(default=0)
 
     def __str__(self):
         return self.address_hash
