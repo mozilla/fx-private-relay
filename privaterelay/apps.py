7a4d2424de350c97cc20e894c7e30f34246aaea4
lloan alas <lloanalas@outlook.com>
2021-09-07 12:03:02 -0700

    add os cross-compatabile method to set gulp develop command

--- privaterelay/apps.py
+++ privaterelay/apps.py
@@ -1,15 +1,19 @@
 import requests
+import os
 
 from django.apps import AppConfig
 from django.conf import settings
-
+ 
+ROOT_DIR = os.path.abspath(os.curdir)
 
 class PrivateRelayConfig(AppConfig):
     name = 'privaterelay'
 
-    settings.GULP_DEVELOP_COMMAND = "node_modules/.bin/gulp"
+    settings.GULP_DEVELOP_COMMAND = ROOT_DIR + "/node_modules/.bin/gulp"
 
     def __init__(self, app_name, app_module):
+         
+
         super(PrivateRelayConfig, self).__init__(app_name, app_module)
         self.fxa_verifying_keys = []
 
