e9b4016b07fc27ee2c4eb6d4d56fb17a6b7eed2e
Vincent <git@vincenttunru.com>
2021-09-17 16:37:45 +0200

    fixup! Add blocklists with commonly-abused names

--- emails/apps.py
+++ emails/apps.py
@@ -22,29 +22,22 @@ class EmailsConfig(AppConfig):
         except Exception:
             logger.exception("exception during SES connect")
 
-        badwords = []
         # badwords file from:
         # https://www.cs.cmu.edu/~biglou/resources/bad-words.txt
-        badwords_file_path = os.path.join(
-            settings.BASE_DIR, 'emails', 'badwords.txt'
-        )
-        with open(badwords_file_path, 'r') as badwords_file:
-            for word in badwords_file:
-                if len(word.strip()) > 0 and word.strip()[0] == "#":
-                    continue
-                badwords.append(word.strip())
-        self.badwords = badwords
+        self.badwords = self._load_terms('badwords.txt')
+        self.blocklist = self._load_terms('blocklist.txt')
 
-        blocklist = []
-        blocklist_file_path = os.path.join(
-            settings.BASE_DIR, 'emails', 'blocklist.txt'
+    def _load_terms(self, filename):
+        terms = []
+        terms_file_path = os.path.join(
+            settings.BASE_DIR, 'emails', filename
         )
-        with open(blocklist_file_path, 'r') as blocklist_file:
-            for word in blocklist_file:
+        with open(terms_file_path, 'r') as terms_file:
+            for word in terms_file:
                 if len(word.strip()) > 0 and word.strip()[0] == "#":
                     continue
-                blocklist.append(word.strip())
-        self.blocklist = blocklist
+                terms.append(word.strip())
+        return terms
 
     def ready(self):
         import emails.signals
