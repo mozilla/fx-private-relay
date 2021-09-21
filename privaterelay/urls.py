98e438b02ef1fd6d72d4815dfa6c9c0699d094fb
Vincent <git@vincenttunru.com>
2021-09-21 17:15:27 +0200

    Start first new landing page design implementation
    This adds a /newlanding/ page that implements one design ("A") for
    a new landing page. The page is split up in several partials to
    make it easier to avoid merge conflicts when working on the
    implementation simultaneously.
    
    It also moves a few globally-applied styles to only apply to pages
    wrapped in .c-body-wrapper.t-dark, because they conflicted with the
    design.
    
    Unfortunately it seems there's still some global styling
    that I've broken, as the links in the header and footer aren't
    styled the way they're supposed to anymore.

--- privaterelay/urls.py
+++ privaterelay/urls.py
@@ -45,6 +45,7 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
+        path('newlanding/', views.newlanding, name='newlanding'),
     ]
 
 if settings.ADMIN_ENABLED:
--- privaterelay/views.py
+++ privaterelay/views.py
@@ -54,6 +54,12 @@ def home(request):
     return render(request, 'home.html')
 
 
+def newlanding(request):
+    # if (request.user and not request.user.is_anonymous):
+    #     return redirect(reverse('profile'))
+    return render(request, 'newlanding.html')
+
+
 def faq(request):
   if (not request.user or request.user.is_anonymous):
     return render(request, 'faq.html')
