7b8d5575bad0cda19a909236e5256d54cd368cf6
groovecoder <lcrouch@mozilla.com>
2021-10-13 15:25:45 -0500

    only call set_cookie if response available

--- privaterelay/signals.py
+++ privaterelay/signals.py
@@ -8,14 +8,14 @@ from emails.utils import incr_if_enabled
 @receiver(user_signed_up)
 def record_user_signed_up(request, user, **kwargs):
     incr_if_enabled('user_signed_up', 1)
-    kwargs.get('response').set_cookie(
-        'server_ga_event', 'user_signed_up', max_age=5
-    )
+    response = kwargs.get('response')
+    if response:
+        response.set_cookie('server_ga_event', 'user_signed_up', max_age=5)
 
 
 @receiver(user_logged_in)
 def record_user_logged_in(request, user, **kwargs):
     incr_if_enabled('user_logged_in', 1)
-    kwargs.get('response').set_cookie(
-        'server_ga_event', 'user_logged_in', max_age=5
-    )
+    response = kwargs.get('response')
+    if response:
+        response.set_cookie('server_ga_event', 'user_logged_in', max_age=5)
