ab47b62484b84b08dc4cca46d2a9f66273a554d3
groovecoder <lcrouch@mozilla.com>
2021-10-14 08:24:03 -0500

    try token auth before session auth

--- privaterelay/settings.py
+++ privaterelay/settings.py
@@ -395,8 +395,8 @@ else:
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSIONS_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
