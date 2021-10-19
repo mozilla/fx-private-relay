b6b02204d4381d5021b3587f2343db5dfbd4ad0f
Vincent <git@vincenttunru.com>
2021-10-19 15:13:18 +0200

    Set up Fluent for the React UI

--- react-ui/next.config.js
+++ react-ui/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.ftl/,
+      type: "asset/source",
+    });
+
+    return config;
+  },
+};
