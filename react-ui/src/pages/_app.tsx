929ba4d0cc41f5e5ee22d102f5585c43480bb9f9
Vincent <git@vincenttunru.com>
2021-10-25 12:04:08 +0200

    Add foundation of the user menu

--- react-ui/src/pages/_app.tsx
+++ react-ui/src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { LocalizationProvider } from "@fluent/react";
+import { SSRProvider } from "@react-aria/ssr";
 import { getL10n } from "../functions/getL10n";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <LocalizationProvider l10n={getL10n()}>
-      <Component {...pageProps} />
-    </LocalizationProvider>
+    <SSRProvider>
+      <LocalizationProvider l10n={getL10n()}>
+        <Component {...pageProps} />
+      </LocalizationProvider>
+    </SSRProvider>
   );
 }
 export default MyApp;
