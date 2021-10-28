f253e86b4824b7711547c88d93850397767b95d0
Vincent <git@vincenttunru.com>
2021-10-28 10:54:29 +0200

    Upgrade to Next.js 12

---
+++ react-ui/.lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "*.{ts,tsx,js,jsx,scss,css,md}": "prettier --write",
+  "*.{ts,tsx,js,jsx}": (filenames) =>
+    `next lint --fix --file ${filenames
+      .map((file) => file.split(process.cwd())[1])
+      .join(" --file ")}`,
+};
