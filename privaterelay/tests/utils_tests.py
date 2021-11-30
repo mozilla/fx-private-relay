e18f17f020500ed2705ca13132a94ae3d1c76297
groovecoder <lcrouch@mozilla.com>
2021-11-30 15:06:15 -0600

    fix get_premium_country_lang and test

---
+++ privaterelay/tests/utils_tests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+from ..utils import (
+    get_premium_country_lang,
+    get_premium_countries_info_from_request
+)
+
+
+class GetPremiumCountryLangTest(TestCase):
+    def test_get_premium_country_lang(self):
+        cc, lang = get_premium_country_lang('en-au,')
+        assert cc == 'au'
+        assert lang == 'en'
+
+        cc, lang = get_premium_country_lang('en-us,')
+        assert cc == 'us'
+        assert lang == 'en'
+
+        cc, lang = get_premium_country_lang('de-be,')
+        assert cc == 'be'
+        assert lang == 'de'
+
+        cc, lang = get_premium_country_lang('de-be,', 'at')
+        assert cc == 'at'
+        assert lang == 'de'
