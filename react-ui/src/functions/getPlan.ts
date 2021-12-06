0417c8e5bb25bdb296088711b7af47653c6590d1
Vincent <git@vincenttunru.com>
2021-12-06 17:02:31 +0100

    Add disabled button if Premium is not available

--- react-ui/src/functions/getPlan.ts
+++ react-ui/src/functions/getPlan.ts
@@ -1,6 +1,8 @@
 import { PremiumCountriesData } from "../hooks/api/premiumCountries";
 
-export const getPlan = (premiumCountriesData: PremiumCountriesData) => {
+export const getPlan = (
+  premiumCountriesData: PremiumCountriesDataWithPremiumAvailable
+) => {
   const languageCode = navigator.language.split("-")[0].toLowerCase();
   const countryPlans =
     premiumCountriesData.plan_country_lang_mapping[
@@ -13,9 +15,25 @@ export const getPlan = (premiumCountriesData: PremiumCountriesData) => {
 };
 
 export const getPremiumSubscribeLink = (
-  premiumCountriesData: PremiumCountriesData
+  premiumCountriesData: PremiumCountriesDataWithPremiumAvailable
 ) => {
   const plan = getPlan(premiumCountriesData);
 
   return `${process.env.NEXT_PUBLIC_FXA_BASE_ORIGIN}subscriptions/products/${process.env.NEXT_PUBLIC_PREMIUM_PROD_ID}?plan=${plan.id}`;
 };
+
+/**
+ * This type ensures that [[getPlan]] and [[getPremiumSubscribeLink]] are not called
+ * unless we can count on a plan being available for them to return
+ * (i.e. after that's been checked with our user-defined type guard
+ * [isPremiumAvailableInCountry]).
+ */
+export type PremiumCountriesDataWithPremiumAvailable = PremiumCountriesData & {
+  premium_available_in_country: true;
+};
+
+export function isPremiumAvailableInCountry(
+  premiumCountriesData: PremiumCountriesData | undefined
+): premiumCountriesData is PremiumCountriesDataWithPremiumAvailable {
+  return premiumCountriesData?.premium_available_in_country === true;
+}
