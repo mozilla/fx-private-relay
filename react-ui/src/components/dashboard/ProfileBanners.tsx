fc9b1ab15d9708d7e87ff863cb84e5e26dfc7cf8
Vincent <git@vincenttunru.com>
2021-12-07 16:06:27 +0100

    Add banner for Firefox

--- react-ui/src/components/dashboard/ProfileBanners.tsx
+++ react-ui/src/components/dashboard/ProfileBanners.tsx
@@ -1,10 +1,13 @@
 import { useLocalization } from "@fluent/react";
 import { ReactNode } from "react";
+import styles from "./ProfileBanners.module.scss";
+import FirefoxLogo from "../../../../static/images/logos/fx-logo.svg";
 import RelayLogo from "../../../../static/images/placeholder-logo.svg";
 import {
   getPremiumSubscribeLink,
   isPremiumAvailableInCountry,
 } from "../../functions/getPlan";
+import { isUsingFirefox } from "../../functions/userAgent";
 import { usePremiumCountries } from "../../hooks/api/premiumCountries";
 import { ProfileData } from "../../hooks/api/profile";
 import { Banner } from "../Banner";
@@ -17,6 +20,25 @@ export const ProfileBanners = (props: Props) => {
   const { l10n } = useLocalization();
   const premiumCountriesData = usePremiumCountries();
   const banners: ReactNode[] = [];
+
+  if (!isUsingFirefox()) {
+    banners.push(
+      <Banner
+        type="promo"
+        title={l10n.getString("banner-download-firefox-headline")}
+        illustration={
+          <img src={FirefoxLogo.src} alt="" width={60} height={60} />
+        }
+        cta={{
+          target:
+            "https://www.mozilla.org/firefox/new/?utm_source=fx-relay&utm_medium=banner&utm_campaign=download-fx",
+          content: l10n.getString("banner-download-firefox-cta"),
+        }}
+      >
+        <p>{l10n.getString("banner-download-firefox-copy")}</p>
+      </Banner>
+    );
+  }
 
   if (
     !props.profile.has_premium &&
@@ -37,5 +59,5 @@ export const ProfileBanners = (props: Props) => {
     );
   }
 
-  return <>{banners}</>;
+  return <div className={styles.profileBanners}>{banners}</div>;
 };
