004795b480f69e912d64f81f7dd766a9f78d9922
Vincent <git@vincenttunru.com>
2021-12-13 13:29:18 +0100

    Allow reuse of SubdomainPicker components

---
+++ react-ui/src/components/dashboard/subdomain/SearchForm.tsx
@@ -0,0 +1,62 @@
+import { useLocalization } from "@fluent/react";
+import { FormEventHandler, useState } from "react";
+import { toast } from "react-toastify";
+import { authenticatedFetch } from "../../../hooks/api/api";
+import { Button } from "../../Button";
+
+export type Props = {
+  onPick: (subdomain: string) => void;
+};
+
+export const SubdomainSearchForm = (props: Props) => {
+  const { l10n } = useLocalization();
+  const [subdomainInput, setSubdomainInput] = useState("");
+
+  const onSubmit: FormEventHandler = async (event) => {
+    event.preventDefault();
+
+    const isAvailable = await getAvailability(subdomainInput);
+    if (!isAvailable) {
+      toast(
+        l10n.getString("error-subdomain-not-available", {
+          unavailable_subdomain: subdomainInput,
+        }),
+        { type: "error" }
+      );
+      return;
+    }
+
+    props.onPick(subdomainInput);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="search"
+        value={subdomainInput}
+        onChange={(e) => setSubdomainInput(e.target.value)}
+        placeholder={l10n.getString(
+          "banner-choose-subdomain-input-placeholder"
+        )}
+        name="subdomain"
+        id="subdomain"
+        minLength={1}
+        maxLength={63}
+      />
+      <Button type="submit">
+        {l10n.getString("banner-register-subdomain-button-search")}
+      </Button>
+    </form>
+  );
+};
+
+async function getAvailability(subdomain: string) {
+  const checkResponse = await authenticatedFetch(
+    `/accounts/profile/subdomain?subdomain=${subdomain}`
+  );
+  if (!checkResponse.ok) {
+    return false;
+  }
+  const checkData: { available: true } = await checkResponse.json();
+  return checkData.available;
+}
