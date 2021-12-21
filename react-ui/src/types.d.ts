434bd8fb8d1caa2479f6d65a4894f3327eb95bc0
Vincent <git@vincenttunru.com>
2021-12-21 13:47:09 +0100

    Add useAddonData()
    This allows us to get information from the add-on, such as whether
    it is installed in the first place, and whether the user is logged
    in there.

---
+++ react-ui/src/types.d.ts
@@ -0,0 +1,9 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    // By default TypeScript will mark custom elements as invalid;
+    // this defines <firefox-private-relay-addon> as equivalent to
+    // a <div>, allowing us to e.g. set data attributes on it.
+    // See https://github.com/Microsoft/TypeScript/issues/4648#issuecomment-137845601.
+    "firefox-private-relay-addon": JSX.IntrinsicElements.div;
+  }
+}
