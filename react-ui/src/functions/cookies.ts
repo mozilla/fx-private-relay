2d932578feebcce871c4b851ff91ede92d389874
Vincent <git@vincenttunru.com>
2021-12-22 15:21:05 +0100

    Add hook to make it easy to allow local dismissal
    The useLocalDismissal hook makes it easy to add e.g. a banner that
    the user should be able to dismiss and then not see for a set
    period of time.

---
+++ react-ui/src/functions/cookies.ts
@@ -0,0 +1,35 @@
+export function getCookie(cookieId: string): string | undefined {
+  if (typeof document === "undefined") {
+    // When server-side rendering:
+    return undefined;
+  }
+  const relevantCookieString = document?.cookie
+    .split(";")
+    .map((cookieString) => cookieString.trim())
+    .find((cookieString) => cookieString.startsWith(cookieId + "="));
+
+  const cookieStringParts = relevantCookieString?.split("=");
+  const cookieValue =
+    Array.isArray(cookieStringParts) && cookieStringParts.length === 2
+      ? cookieStringParts[1]
+      : undefined;
+
+  return cookieValue;
+}
+
+export type SetCookieOptions = {
+  /** How many seconds until the browser should delete the cookie. */
+  maxAgeInSeconds?: number;
+};
+export function setCookie(
+  cookieId: string,
+  value: string,
+  options: SetCookieOptions = {}
+) {
+  const maxAgeString =
+    typeof options.maxAgeInSeconds === "number"
+      ? `;max-age=${options.maxAgeInSeconds.toString()}`
+      : "";
+
+  document.cookie = `${cookieId}=${value}; SameSite=Strict; path=/${maxAgeString}`;
+}
---
+++ react-ui/src/hooks/localDismissal.ts
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { getCookie, setCookie } from "../functions/cookies";
+
+export type DismissalData = {
+  isDismissed: boolean;
+  dismiss: () => void;
+};
+
+export type DismissalOptions = {
+  /** How long the dismissal should last, in seconds. Note that by default, the cookie will be deleted in 30 days. */
+  duration?: number;
+};
+
+/**
+ * This can be used to store a cookie to remember that something was dismissed.
+ *
+ * @param key Key to identity the item-to-be-dismissed with (i.e. to use in the cookie name). Tip: incorporate the user's ID to make a dismissal user-specific.
+ * @param options See {@see DismissalOptions}.
+ * @returns Whether the item has been dismissed yet, and a function to call to dismiss the item.
+ */
+export function useLocalDismissal(
+  key: string,
+  options: DismissalOptions = {}
+): DismissalData {
+  const cookieId = key + "_dismissed";
+  const dismissalCookieValue = getCookie(cookieId);
+  const dismissalTimeStamp = dismissalCookieValue
+    ? Number.parseInt(dismissalCookieValue, 10)
+    : undefined;
+
+  const wasDismissedBefore =
+    // To be dismissed, the cookie has to be set, and either...
+    typeof dismissalTimeStamp === "number" &&
+    //   ...the dismissal should not be limited in duration, or...
+    (typeof options.duration !== "number" ||
+      //   ...the dismissal was long enough ago:
+      Date.now() - dismissalTimeStamp <= options.duration * 1000);
+
+  const [isDismissed, setIsDismissed] = useState(wasDismissedBefore);
+
+  const dismiss = () => {
+    const maxAgeInSeconds =
+      typeof options.duration === "number"
+        ? options.duration
+        : 30 * 24 * 60 * 60;
+    setCookie(cookieId, Date.now().toString(), {
+      maxAgeInSeconds: maxAgeInSeconds,
+    });
+    setIsDismissed(true);
+  };
+
+  return {
+    isDismissed: isDismissed,
+    dismiss: dismiss,
+  };
+}
