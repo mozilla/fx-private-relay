c66ec6713f299737ee7b8788e4b3c9dff30223e0
Vincent <git@vincenttunru.com>
2021-12-22 15:29:01 +0100

    Add useFirstSeen hook
    This can be used to track when useres were first seen. Note that it
    only tracks when user first visited a page *that includes this
    hook*.

---
+++ react-ui/src/hooks/firstSeen.ts
@@ -0,0 +1,36 @@
+import { getCookie, setCookie } from "../functions/cookies";
+import { useProfiles } from "./api/profile";
+import { useIsLoggedIn } from "./session";
+
+/**
+ * Keep track of when the logged-in user first encountered a page *with this hook*
+ *
+ * If a component using this hook is first rendered and the user is logged in,
+ * a cookie is set storing the current date and time. The hook will then keep
+ * returning that date and time for the current user.
+ *
+ * @returns Date and time when the current user was first seen.
+ */
+export function useFirstSeen(): Date | null {
+  const isLoggedIn = useIsLoggedIn();
+  const profileData = useProfiles();
+
+  if (!isLoggedIn || !profileData.data?.[0].id) {
+    return null;
+  }
+
+  const firstSeenString = getCookie("first_seen_" + profileData.data[0].id);
+  if (typeof firstSeenString === "string") {
+    return new Date(Number.parseInt(firstSeenString, 10));
+  }
+
+  const currentTimestamp = Date.now();
+  setCookie(
+    "first_seen_" + profileData.data[0].id,
+    currentTimestamp.toString(),
+    {
+      maxAgeInSeconds: 10 * 365 * 24 * 60 * 60,
+    }
+  );
+  return new Date(currentTimestamp);
+}
