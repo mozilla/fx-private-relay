a5b1af3027a4da22f09e0e3e2e96ed098bff0525
John Whitlock <jwhitlock@mozilla.com>
2022-01-05 10:02:28 -0600

    Remove unused test code

--- api/tests/views_tests.py
+++ api/tests/views_tests.py
@@ -2,15 +2,12 @@ from django.test import TestCase
 
 import pytest
 
+
 @pytest.mark.parametrize("format", ("yaml", "json"))
 def test_swagger_format(client, format):
     path = f"/api/v1/swagger.{format}"
     response = client.get(path)
     assert response.status_code == 200
-    expected_content_type = {
-        "yaml": "application/yaml; charset=utf-8",
-        "json": "application/json; charset=utf-8",
-    }[format]
     assert response["Content-Type"].startswith(f"application/{format}")
 
 
