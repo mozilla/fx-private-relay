6541490bfddc5d05a166b31ca8d0b08438287cc8
Se Yeon Kim <sykim@mozilla.com>
2022-01-19 09:41:19 -0600

    Reference the correct migration as a dependency

--- emails/migrations/0038_domain_address_min_length_validator_and_unique_together_user_and_address.py
+++ emails/migrations/0038_domain_address_min_length_validator_and_unique_together_user_and_address.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('emails', '0037_domain_address_min_length_validator_and_unique_together_user_and_address'),
+        ('emails', '0037_reply_add_index_on_created_at'),
     ]
 
     operations = [
