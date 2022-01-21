04c5f6a914050ecd073657f69935b67fe93c9567
groovecoder <lcrouch@mozilla.com>
2022-01-21 09:59:54 -0600

    add pytest-codecov and circle config to publish coverage reports

--- api/tests/views_tests.py
+++ api/tests/views_tests.py
@@ -1,5 +1,3 @@
-from django.test import TestCase
-
 import pytest
 
 
@@ -10,9 +8,13 @@ def test_swagger_format(client, format):
     assert response.status_code == 200
     assert response["Content-Type"].startswith(f"application/{format}")
 
-
 @pytest.mark.parametrize("subpath", ("swagger", "swagger.", "swagger.txt"))
 def test_swagger_unknown_format(client, subpath):
     path = f"/api/v1/{subpath}"
     response = client.get(path)
     assert response.status_code == 404
+
+def test_premium_countries(client):
+    path = '/api/v1/premium_countries'
+    response = client.get(path)
+    assert response.status_code == 200
