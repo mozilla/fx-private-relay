537451a7459fb5fbdf4806a86b35cf3987357ff4
Vincent <git@vincenttunru.com>
2022-02-07 13:40:34 +0100

    Set up Stylelint for the React frontend
    This copies over the existing Stylelint config, with some
    modifications to not clash with Prettier and some Next.js-specific
    SCSS pecularities.

---
+++ frontend/.stylelintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  "extends": [
+    "stylelint-config-recommended-scss",
+    "stylelint-config-prettier-scss"
+  ],
+  "plugins": [
+    "stylelint-scss"
+  ],
+  "ignoreFiles": [
+    "src/styles/fonts/**",
+    // This file uses some Next.js-specific syntax to make SCSS variables
+    // available to JS. See
+    // https://nextjs.org/docs/basic-features/built-in-css-support#sass-variables
+    "src/hooks/mediaQuery.module.scss",
+  ],
+  "rules": {
+    "at-rule-empty-line-before": [
+      "always",
+      {
+        "except": ["after-same-name", "first-nested"],
+        "ignore": ["after-comment"]
+      }
+    ],
+    "at-rule-no-unknown": null,
+    "block-opening-brace-newline-after": "always",
+    "block-closing-brace-newline-after": "always",
+    "declaration-block-semicolon-newline-after": "always",
+    "declaration-colon-space-after": "always-single-line",
+    "declaration-colon-space-before": "never",
+    "function-url-quotes": "always",
+    "no-descending-specificity": null,
+    "no-missing-end-of-source-newline": true,
+    "scss/at-rule-no-unknown": true,
+    "scss/at-import-partial-extension": null,
+    "scss/at-import-partial-extension-whitelist": "scss"
+  }
+};
