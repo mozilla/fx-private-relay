365b0359b3c53953fe6b8be86b1153cc8edb2737
Vincent <git@vincenttunru.com>
2022-02-25 13:18:40 +0100

    Allow enabling promo-blocking on alias creation

--- frontend/src/components/dashboard/aliases/AliasList.tsx
+++ frontend/src/components/dashboard/aliases/AliasList.tsx
@@ -19,7 +19,9 @@ export type Props = {
   user: UserData;
   runtimeData?: RuntimeData;
   onCreate: (
-    options: { mask_type: "random" } | { mask_type: "custom"; address: string }
+    options:
+      | { mask_type: "random" }
+      | { mask_type: "custom"; address: string; blockPromotionals: boolean }
   ) => void;
   onUpdate: (alias: AliasData, updatedFields: Partial<AliasData>) => void;
   onDelete: (alias: AliasData) => void;
