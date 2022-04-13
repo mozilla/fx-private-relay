973fc0668f1266f6b38122d69b055efbb6477406
Vincent <git@vincenttunru.com>
2022-04-13 12:44:21 +0200

    Properly mock logout
    On the production website, a logout redirects the user to Firefox
    Accounts, thereby inducing a full page refresh. However, when
    mocking that logout flow locally, we remain in the context of the
    Single-Page App, and therefore user data remained cached in memory.
    This change forces a revalidation of user data after clearing the
    session key, which then results in that data no longer being
    available and thus the user's logged-out status being properly
    recognised.
    
    It also more thoroughly detects whether the user is logged in on
    the homepage, by not only checking if user data is available, but
    also that refetching it did not result in an error (which it does
    if the user is logged out).

--- frontend/src/pages/mock/logout.page.tsx
+++ frontend/src/pages/mock/logout.page.tsx
@@ -3,10 +3,14 @@ import { useRouter } from "next/router";
 import { MouseEventHandler } from "react";
 import { getRuntimeConfig } from "../../config";
 import { authenticatedFetch } from "../../hooks/api/api";
+import { useProfiles } from "../../hooks/api/profile";
+import { useUsers } from "../../hooks/api/user";
 import styles from "./mockSession.module.scss";
 
 const MockLogout: NextPage = () => {
   const router = useRouter();
+  const profiles = useProfiles();
+  const users = useUsers();
 
   const onLogout: MouseEventHandler = async (event) => {
     event.preventDefault();
@@ -19,6 +23,9 @@ const MockLogout: NextPage = () => {
       method: "GET",
       redirect: "manual",
     });
+    // Revalidate account data to reflect logged out status:
+    await profiles.mutate();
+    await users.mutate();
 
     router.push("/");
   };
