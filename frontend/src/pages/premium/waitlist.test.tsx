a9722eb822982fe3a4b599f9015a2bd1badb0c96
Vincent <git@vincenttunru.com>
2022-04-29 13:45:23 +0200

    Add test for the waitlist page

--- frontend/src/pages/premium/waitlist.test.tsx
+++ frontend/src/pages/premium/waitlist.test.tsx
@@ -54,6 +54,30 @@ describe("The waitlist", () => {
     });
   });
 
+  it("sends the user's email to Basket", async () => {
+    render(<PremiumWaitlist />);
+
+    const emailInput = screen.getByLabelText(
+      "l10n string: [waitlist-control-email-label], with vars: {}"
+    );
+    userEvent.clear(emailInput);
+    userEvent.type(emailInput, "some_email@example.com");
+
+    const submitButton = screen.getByRole("button", {
+      name: "l10n string: [waitlist-submit-label], with vars: {}",
+    });
+    userEvent.click(submitButton);
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(
+      "https://basket.mozilla.org/news/subscribe/",
+      expect.objectContaining({ body: expect.stringContaining("") })
+    );
+    const requestBody = (global.fetch as jest.Mock).mock.calls[0][1].body;
+    const params = new URLSearchParams(requestBody);
+    expect(params.get("email")).toBe("some_email@example.com");
+  });
+
   it("uses the Basket URL set by the back-end", async () => {
     setMockRuntimeData({ BASKET_ORIGIN: "https://some-basket-url.com" });
     render(<PremiumWaitlist />);
