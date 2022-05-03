8060fc4eb823de715323780c1918e8ff0a49f87c
Vincent <git@vincenttunru.com>
2022-05-03 09:37:02 +0200

    Deduplicate signin URL generation
    As suggested at
    https://github.com/mozilla/fx-private-relay/pull/1880#discussion_r863195672

--- frontend/__mocks__/hooks/fxaFlowTracker.ts
+++ frontend/__mocks__/hooks/fxaFlowTracker.ts
@@ -3,4 +3,5 @@ export const mockUseFxaFlowTrackerModule = {
     // Tracker data can be undefined while runtime data is still loading:
     return {};
   }),
+  getLoginUrl: jest.fn(() => "https://login-mock.com"),
 };
