e30484354155811e036cf47c9bb64fb96570b223
lloan alas <lloanalas@outlook.com>
2022-05-26 23:28:33 -0700

    added star icon, filler content - ratings section wip

--- frontend/src/components/landing/Reviews.tsx
+++ frontend/src/components/landing/Reviews.tsx
@@ -1,5 +1,6 @@
 import { useLocalization } from "@fluent/react";
 import FxBrowserLogo from "../../../../static/scss/libs/protocol/img/logos/firefox/browser/logo.svg";
+import { StarIcon } from "../Icons";
 import styles from "./Reviews.module.scss";
 
 /**
@@ -20,7 +21,19 @@ export const Reviews = () => {
             {l10n.getString("reviews-add-ons")}
           </p>
         </div>
-        <div className={styles["review-rating"]}></div>
+        <div className={styles["rating-container"]}>
+          <div className={styles.stars}>
+            <StarIcon className={styles.star} alt="" />
+            <StarIcon className={styles.star} alt="" />
+            <StarIcon className={styles.star} alt="" />
+            <StarIcon className={styles.star} alt="" />
+            <StarIcon className={styles["empty-star"]} alt="" />
+          </div>
+          <div className={styles.rating}>
+            <p className={styles.title}>4.2</p>
+            <p className={styles.text}>out of 5 (328 reviews)</p>
+          </div>
+        </div>
         <div className={styles.review}></div>
       </div>
     </section>
