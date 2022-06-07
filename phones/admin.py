432f1b0a1e00eb690f5cd9c54a3d9401b3d1abaf
groovecoder <lcrouch@mozilla.com>
2022-06-06 21:30:03 -0500

    start inbound_sms API endpoint

--- phones/admin.py
+++ phones/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 
-from .models import Session
+from .models import Session, RealPhone, RelayNumber
 
 
-class SessionAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Session, SessionAdmin)
+admin.site.register(Session, admin.ModelAdmin)
+admin.site.register(RealPhone, admin.ModelAdmin)
+admin.site.register(RelayNumber, admin.ModelAdmin)
