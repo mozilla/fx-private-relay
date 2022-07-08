96cd75636226688c265bf954566deb7e3d53c64a
Kaitlyn <kandres@mozilla.com>
2022-07-08 10:54:48 -0400

    refactor whats new entries

--- frontend/src/components/layout/navigation/whatsnew/WhatsNewMenu.tsx
+++ frontend/src/components/layout/navigation/whatsnew/WhatsNewMenu.tsx
@@ -48,6 +48,7 @@ import { isUsingFirefox } from "../../../../functions/userAgent";
 import { getLocale } from "../../../../functions/getLocale";
 import { RuntimeData } from "../../../../hooks/api/runtimeData";
 import { isFlagActive } from "../../../../functions/waffle";
+import { WhatsNewEntries } from "./WhatsNewEntries";
 
 export type WhatsNewEntry = {
   title: string;
@@ -86,256 +87,6 @@ export const WhatsNewMenu = (props: Props) => {
   });
   const triggerRef = useRef<HTMLButtonElement>(null);
   const overlayRef = useRef<HTMLDivElement>(null);
-  const addonData = useAddonData();
-
-  const entries: WhatsNewEntry[] = [
-    {
-      title: l10n.getString("whatsnew-feature-size-limit-heading"),
-      snippet: l10n.getString("whatsnew-feature-size-limit-snippet-var", {
-        size: 10,
-        unit: "MB",
-      }),
-      content: (
-        <WhatsNewContent
-          description={l10n.getString(
-            "whatsnew-feature-size-limit-description-var",
-            {
-              size: 10,
-              unit: "MB",
-            }
-          )}
-          heading={l10n.getString("whatsnew-feature-size-limit-heading")}
-          image={SizeLimitHero.src}
-          videos={{
-            // Unfortunately video files cannot currently be imported, so make
-            // sure these files are present in /public. See
-            // https://github.com/vercel/next.js/issues/35248
-            "video/webm; codecs='vp9'":
-              "/animations/whatsnew/size-limit-hero-10mb.webm",
-            "video/mp4": "/animations/whatsnew/size-limit-hero-10mb.mp4",
-          }}
-        />
-      ),
-      hero: SizeLimitHero.src,
-      icon: SizeLimitIcon.src,
-      dismissal: useLocalDismissal(
-        `whatsnew-feature_size-limit_${props.profile.id}`
-      ),
-      announcementDate: {
-        year: 2022,
-        month: 3,
-        day: 1,
-      },
-    },
-  ];
-  const forwardSomeEntry: WhatsNewEntry = {
-    title: l10n.getString("whatsnew-feature-forward-some-heading"),
-    snippet: l10n.getString("whatsnew-feature-forward-some-snippet"),
-    content: (
-      <WhatsNewContent
-        description={l10n.getString(
-          "whatsnew-feature-forward-some-description"
-        )}
-        heading={l10n.getString("whatsnew-feature-forward-some-heading")}
-        image={ForwardSomeHero.src}
-      />
-    ),
-    hero: ForwardSomeHero.src,
-    icon: ForwardSomeIcon.src,
-    dismissal: useLocalDismissal(
-      `whatsnew-feature_sign-back-in_${props.profile.id}`
-    ),
-    announcementDate: {
-      year: 2022,
-      month: 3,
-      day: 1,
-    },
-  };
-  if (props.profile.has_premium) {
-    entries.push(forwardSomeEntry);
-  }
-
-  const signBackInEntry: WhatsNewEntry = {
-    title: l10n.getString("whatsnew-feature-sign-back-in-heading"),
-    snippet: l10n.getString("whatsnew-feature-sign-back-in-snippet"),
-    content: (
-      <WhatsNewContent
-        description={l10n.getString(
-          "whatsnew-feature-sign-back-in-description"
-        )}
-        heading={l10n.getString("whatsnew-feature-sign-back-in-heading")}
-        image={SignBackInHero.src}
-      />
-    ),
-    hero: SignBackInHero.src,
-    icon: SignBackInIcon.src,
-    dismissal: useLocalDismissal(
-      `whatsnew-feature_sign-back-in_${props.profile.id}`
-    ),
-    announcementDate: {
-      year: 2022,
-      month: 2,
-      day: 1,
-    },
-  };
-  if (addonData.present && isUsingFirefox()) {
-    entries.push(signBackInEntry);
-  }
-
-  const aliasToMask: WhatsNewEntry = {
-    title: l10n.getString("whatsnew-feature-alias-to-mask-heading"),
-    snippet: l10n.getString("whatsnew-feature-alias-to-mask-snippet"),
-    content: (
-      <WhatsNewContent
-        description={l10n.getString(
-          "whatsnew-feature-alias-to-mask-description"
-        )}
-        heading={l10n.getString("whatsnew-feature-alias-to-mask-heading")}
-        image={aliasToMaskHero.src}
-      />
-    ),
-    hero: aliasToMaskHero.src,
-    icon: aliasToMaskIcon.src,
-    dismissal: useLocalDismissal(
-      `whatsnew-feature_alias-to-mask_${props.profile.id}`
-    ),
-    announcementDate: {
-      year: 2022,
-      month: 4,
-      day: 19,
-    },
-  };
-  // Not all localisations transitioned from "alias" to "mask", so only show this
-  // announcement for those of which we _know_ did:
-  if (
-    [
-      "en",
-      "en-gb",
-      "nl",
-      "fy-nl",
-      "zh-tw",
-      "es-es",
-      "es-mx",
-      "de",
-      "pt-br",
-      "sv-se",
-      "el",
-      "hu",
-      "sk",
-      "skr",
-      "uk",
-    ].includes(getLocale(l10n).toLowerCase())
-  ) {
-    entries.push(aliasToMask);
-  }
-
-  const premiumInSweden: WhatsNewEntry = {
-    title: l10n.getString("whatsnew-feature-premium-expansion-sweden-heading"),
-    snippet: l10n.getString("whatsnew-feature-premium-expansion-snippet"),
-    content: (
-      <WhatsNewContent
-        description={l10n.getString(
-          "whatsnew-feature-premium-expansion-description"
-        )}
-        heading={l10n.getString(
-          "whatsnew-feature-premium-expansion-sweden-heading"
-        )}
-        image={PremiumSwedenHero.src}
-      />
-    ),
-    hero: PremiumSwedenHero.src,
-    icon: PremiumSwedenIcon.src,
-    dismissal: useLocalDismissal(
-      `whatsnew-feature_premium-expansion-sweden_${props.profile.id}`
-    ),
-    announcementDate: {
-      year: 2022,
-      month: 5,
-      day: 17,
-    },
-  };
-  if (
-    props.runtimeData?.PREMIUM_PLANS.country_code.toLowerCase() === "se" &&
-    !props.profile.has_premium
-  ) {
-    entries.push(premiumInSweden);
-  }
-
-  const premiumInFinland: WhatsNewEntry = {
-    title: l10n.getString("whatsnew-feature-premium-expansion-finland-heading"),
-    snippet: l10n.getString("whatsnew-feature-premium-expansion-snippet"),
-    content: (
-      <WhatsNewContent
-        description={l10n.getString(
-          "whatsnew-feature-premium-expansion-description"
-        )}
-        heading={l10n.getString(
-          "whatsnew-feature-premium-expansion-finland-heading"
-        )}
-        image={PremiumFinlandHero.src}
-      />
-    ),
-    hero: PremiumFinlandHero.src,
-    icon: PremiumFinlandIcon.src,
-    dismissal: useLocalDismissal(
-      `whatsnew-feature_premium-expansion-finland_${props.profile.id}`
-    ),
-    announcementDate: {
-      year: 2022,
-      month: 5,
-      day: 17,
-    },
-  };
-  if (
-    props.runtimeData?.PREMIUM_PLANS.country_code.toLowerCase() === "fi" &&
-    !props.profile.has_premium
-  ) {
-    entries.push(premiumInFinland);
-  }
-
-  const trackerRemoval: WhatsNewEntry = {
-    title: l10n.getString("whatsnew-feature-tracker-removal-heading"),
-    snippet: l10n.getString("whatsnew-feature-tracker-removal-snippet"),
-    content: (
-      <WhatsNewContent
-        description={l10n.getString(
-          "whatsnew-feature-tracker-removal-description"
-        )}
-        heading={l10n.getString("whatsnew-feature-tracker-removal-heading")}
-        image={TrackerRemovalHero.src}
-      />
-    ),
-    hero: TrackerRemovalHero.src,
-    icon: TrackerRemovalIcon.src,
-    dismissal: useLocalDismissal(
-      `whatsnew-feature_tracker-removal_${props.profile.id}`
-    ),
-    announcementDate: {
-      year: 2022,
-      month: 6,
-      day: 23,
-    },
-  };
-  // Only show its announcement if tracker removal is live:
-  if (isFlagActive(props.runtimeData, "tracker_removal")) {
-    entries.push(trackerRemoval);
-  }
-
-  entries.sort(entriesDescByDateSorter);
-
-  const newEntries = entries.filter((entry) => {
-    const entryDate = new Date(
-      Date.UTC(
-        entry.announcementDate.year,
-        entry.announcementDate.month - 1,
-        entry.announcementDate.day
-      )
-    );
-    const ageInMilliSeconds = Date.now() - entryDate.getTime();
-    // Automatically move entries to the archive after 30 days:
-    const isExpired = ageInMilliSeconds > 30 * 24 * 60 * 60 * 1000;
-    return !entry.dismissal.isDismissed && !isExpired;
-  });
 
   const { triggerProps, overlayProps } = useOverlayTrigger(
     { type: "dialog" },
@@ -356,22 +107,6 @@ export const WhatsNewMenu = (props: Props) => {
     triggerRef
   );
 
-  if (entries.length === 0) {
-    return null;
-  }
-
-  const pill =
-    newEntries.length > 0 ? (
-      <i
-        aria-label={l10n.getString("whatsnew-counter-label", {
-          count: newEntries.length,
-        })}
-        className={styles.pill}
-      >
-        {newEntries.length}
-      </i>
-    ) : null;
-
   return (
     <>
       <button
@@ -383,7 +118,7 @@ export const WhatsNewMenu = (props: Props) => {
         } ${props.style}`}
       >
         {l10n.getString("whatsnew-trigger-label")}
-        {pill}
+        {/* {pill} */}
       </button>
       {triggerState.isOpen && (
         <OverlayContainer>
@@ -395,11 +130,12 @@ export const WhatsNewMenu = (props: Props) => {
             isOpen={triggerState.isOpen}
             onClose={() => triggerState.close()}
           >
-            <WhatsNewDashboard
+            {/* <WhatsNewDashboard
               new={newEntries}
               archive={entries}
               onClose={() => triggerState.close()}
-            />
+            /> */}
+            <WhatsNewEntries />
           </WhatsNewPopover>
         </OverlayContainer>
       )}
