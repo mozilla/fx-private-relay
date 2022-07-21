4d89b92ffe68c7bfde042aa2dc02c0d9397f8d4f
Se Yeon Kim <sykim@mozilla.com>
2022-07-21 17:17:35 -0500

    Make read only for tracker removal count fields

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -57,6 +57,7 @@ class RelayAddressSerializer(PremiumValidatorsMixin, serializers.ModelSerializer
             "last_used_at",
             "num_forwarded",
             "num_blocked",
+            "num_level_one_trackers_blocked",
             "num_replied",
             "num_spam",
         ]
@@ -97,6 +98,7 @@ class DomainAddressSerializer(PremiumValidatorsMixin, serializers.ModelSerialize
             "last_used_at",
             "num_forwarded",
             "num_blocked",
+            "num_level_one_trackers_blocked",
             "num_replied",
             "num_spam",
         ]
@@ -135,6 +137,7 @@ class ProfileSerializer(serializers.ModelSerializer):
             "emails_blocked",
             "emails_forwarded",
             "emails_replied",
+            "level_one_trackers_blocked",
         ]
 
 
