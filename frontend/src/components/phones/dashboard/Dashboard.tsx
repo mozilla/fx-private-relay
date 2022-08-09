d7ca6a430e8e18c5f59ca5abb4ed1e24e0d909dc
Kaitlyn <kandres@mozilla.com>
2022-08-09 15:15:39 -0700

    Add copy functionality

--- frontend/src/components/phones/dashboard/Dashboard.tsx
+++ frontend/src/components/phones/dashboard/Dashboard.tsx
@@ -1,8 +1,17 @@
 import { useRelayNumber } from "../../../hooks/api/relayNumber";
 import styles from "./PhoneDashboard.module.scss";
+import { CopyIcon } from "../../../components/Icons";
+import { useRef, useState } from "react";
 
 export const PhoneDashboard = () => {
   const relayNumberData = useRelayNumber();
+  const [justCopiedApiKey, setJustCopiedApiKey] = useState(false);
+
+  const copyPhoneNumber: MouseEventHandler<HTMLButtonElement> = () => {
+    navigator.clipboard.writeText("Add phone number");
+    setJustCopiedApiKey(true);
+    setTimeout(() => setJustCopiedApiKey(false), 1000);
+  };
 
   return (
     <main>
@@ -12,6 +21,31 @@ export const PhoneDashboard = () => {
         <p className={styles["header-phone-number"]}>
           {relayNumberData.data?.[0]?.number}
         </p>
+        <span className={styles["copy-controls"]}>
+          <span className={styles["copy-button-wrapper"]}>
+            <button
+              type="button"
+              className={styles["copy-button"]}
+              title="Copied"
+              onClick={copyPhoneNumber}
+            >
+              <CopyIcon
+                alt="test"
+                className={styles["copy-icon"]}
+                width={24}
+                height={24}
+              />
+            </button>
+            <span
+              aria-hidden={!justCopiedApiKey}
+              className={`${styles["copied-confirmation"]} ${
+                justCopiedApiKey ? styles["is-shown"] : ""
+              }`}
+            >
+              Copied!
+            </span>
+          </span>
+        </span>
 
         <div className={styles["phone-data-cards-container"]}>
           <div className={styles["phone-data-card"]}>
