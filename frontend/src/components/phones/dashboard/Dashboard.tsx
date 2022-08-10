a3d3fafb878398126626d76077fc59b63174e5c6
Kaitlyn <kandres@mozilla.com>
2022-08-10 10:28:38 -0700

    add dashboard controls

--- frontend/src/components/phones/dashboard/Dashboard.tsx
+++ frontend/src/components/phones/dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 import { useRelayNumber } from "../../../hooks/api/relayNumber";
 import styles from "./PhoneDashboard.module.scss";
-import { CopyIcon } from "../../../components/Icons";
+import { CopyIcon, ForwardIcon, BlockIcon } from "../../../components/Icons";
 import { MouseEventHandler, useState } from "react";
 
 export const PhoneDashboard = () => {
@@ -8,10 +9,71 @@ export const PhoneDashboard = () => {
   const [justCopiedApiKey, setJustCopiedPhoneNumber] = useState(false);
 
   const copyPhoneNumber: MouseEventHandler<HTMLButtonElement> = () => {
-    navigator.clipboard.writeText(relayNumberData.data?.[0]?.number);
+    // TODO: Find a way to not have to use a non-null assertion operator here
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    navigator.clipboard.writeText(relayNumberData?.data?.[0].number!);
     setJustCopiedPhoneNumber(true);
     setTimeout(() => setJustCopiedPhoneNumber(false), 1000);
   };
+
+  const phoneStatistics = (
+    <div className={styles["phone-statistics-container"]}>
+      <div className={styles["phone-statistics"]}>
+        <p className={styles["phone-statistics-title"]}>12:04 min</p>
+        <p className={styles["phone-statistics-body"]}>
+          Remaining call minutes
+        </p>
+      </div>
+
+      <div className={styles["phone-statistics"]}>
+        <p className={styles["phone-statistics-title"]}>12:04 min</p>
+        <p className={styles["phone-statistics-body"]}>
+          Remaining call minutes
+        </p>
+      </div>
+
+      <div className={styles["phone-statistics"]}>
+        <p className={styles["phone-statistics-title"]}>12:04 min</p>
+        <p className={styles["phone-statistics-body"]}>
+          Remaining call minutes
+        </p>
+      </div>
+
+      <div className={styles["phone-statistics"]}>
+        <p className={styles["phone-statistics-title"]}>12:04 min</p>
+        <p className={styles["phone-statistics-body"]}>
+          Remaining call minutes
+        </p>
+      </div>
+    </div>
+  );
+
+  const phoneControls = (
+    <div className={styles["phone-controls-container"]}>
+      <div className={styles["phone-controls"]}>
+        <button className={styles["phone-controls-button"]}>
+          <ForwardIcon
+            alt="Forwarding All Messages"
+            className={styles["forward-icon"]}
+            width={15}
+            height={15}
+          />
+        </button>
+        <button className={styles["phone-controls-button"]}>
+          <BlockIcon
+            alt="Blocking All Messages"
+            className={styles["block-icon"]}
+            width={15}
+            height={15}
+          />
+        </button>
+      </div>
+      <div className={styles["phone-controls-description"]}>
+        Relay is currently forwarding all phone calls and SMS messages to your
+        true phone number.
+      </div>
+    </div>
+  );
 
   return (
     <main>
@@ -47,35 +109,8 @@ export const PhoneDashboard = () => {
           </span>
         </span>
 
-        <div className={styles["phone-data-cards-container"]}>
-          <div className={styles["phone-data-card"]}>
-            <p className={styles["phone-data-card-title"]}>12:04 min</p>
-            <p className={styles["phone-data-card-body"]}>
-              Remaining call minutes
-            </p>
-          </div>
-
-          <div className={styles["phone-data-card"]}>
-            <p className={styles["phone-data-card-title"]}>12:04 min</p>
-            <p className={styles["phone-data-card-body"]}>
-              Remaining call minutes
-            </p>
-          </div>
-
-          <div className={styles["phone-data-card"]}>
-            <p className={styles["phone-data-card-title"]}>12:04 min</p>
-            <p className={styles["phone-data-card-body"]}>
-              Remaining call minutes
-            </p>
-          </div>
-
-          <div className={styles["phone-data-card"]}>
-            <p className={styles["phone-data-card-title"]}>12:04 min</p>
-            <p className={styles["phone-data-card-body"]}>
-              Remaining call minutes
-            </p>
-          </div>
-        </div>
+        {phoneStatistics}
+        {phoneControls}
       </div>
     </main>
   );
