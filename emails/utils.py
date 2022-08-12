92a3104620b35749533b7606af1281a41790062a
John Whitlock <jwhitlock@mozilla.com>
2022-08-11 21:48:50 -0500

    MPP-1921: Expect only Received to have S3 data
    Only Received notifications are expected to have receipt actions and
    S3-stored data (the email contents), so return an empty repsonse early
    for the other message types.

--- emails/utils.py
+++ emails/utils.py
@@ -10,7 +10,7 @@ from email.utils import parseaddr
 from enum import Enum
 import json
 import re
-from typing import Optional, Union
+from typing import Any, Optional, Union
 
 from botocore.exceptions import ClientError
 from cryptography.hazmat.primitives import hashes
@@ -308,24 +308,27 @@ def decrypt_reply_metadata(key, jwe):
     return e.plaintext
 
 
-def _get_bucket_and_key_from_s3_json(message_json):
-    bucket = None
-    object_key = None
+def _get_bucket_and_key_from_s3_json(
+    message_json: dict[str, Any]
+) -> Union[tuple[str, str], tuple[None, None]]:
+    """Get the bucket and object key of the S3-stored email."""
+
+    # Only Received notifications have S3-stored data
+    notification_type = message_json.get("notificationType")
+    if notification_type != "Received":
+        return None, None
+
     if "receipt" in message_json and "action" in message_json["receipt"]:
         message_json_receipt = message_json["receipt"]
     else:
-        notification_type = message_json.get("notificationType")
-        event_type = message_json.get("eventType")
-        is_bounce_notification = notification_type == "Bounce" or event_type == "Bounce"
-        if not is_bounce_notification:
-            # TODO: sns inbound notification does not have 'receipt'
-            # we need to look into this more
-            logger.error(
-                "sns_inbound_message_without_receipt",
-                extra={"message_json_keys": message_json.keys()},
-            )
+        logger.error(
+            "sns_inbound_message_without_receipt",
+            extra={"message_json_keys": message_json.keys()},
+        )
         return None, None
 
+    bucket = None
+    object_key = None
     try:
         if "S3" in message_json_receipt["action"]["type"]:
             bucket = message_json_receipt["action"]["bucketName"]
@@ -333,9 +336,7 @@ def _get_bucket_and_key_from_s3_json(message_json):
     except (KeyError, TypeError) as e:
         logger.error(
             "sns_inbound_message_receipt_malformed",
-            extra={
-                "receipt_action": message_json_receipt["action"],
-            },
+            extra={"receipt_action": message_json_receipt["action"]},
         )
     return bucket, object_key
 
