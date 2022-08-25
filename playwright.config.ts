d2809a49e789902fb2b7f8cf5208a49f9bcd3dcb
Raphael Okafor Jr <rokafor@mozilla.com>
2022-08-25 18:32:01 -0400

    wip updating reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,13 +46,12 @@ const config: PlaywrightTestConfig = {
   reporter: [
     ['line'],
     ['html'],
-    [process.env.CI ?? 'allure-playwright']
-    // ['json', {  outputFile: 'test-results.json' }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
+
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.E2E_TEST_BASE_URL || 'https://stage.fxprivaterelay.nonprod.cloudops.mozgcp.net',
 
