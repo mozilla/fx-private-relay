30506726ce92d9ac365814d6b24b33e269aff468
John Whitlock <jwhitlock@mozilla.com>
2022-08-26 09:40:18 -0500

    Fix frontend linting patterns and "next lint" step

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "frontend/*.{scss,css}": "stylelint --fix",
-  "frontend/*.{ts,tsx,js,jsx,scss,css}": "prettier --write",
-  "frontend/*.{ts,tsx,js,jsx}": (filenames) =>
-    `next lint --fix --file ${filenames
+  "frontend/**/*.{scss,css}": "stylelint --fix",
+  "frontend/**/*.{ts,tsx,js,jsx,scss,css}": "prettier --write",
+  "frontend/**/*.{ts,tsx,js,jsx}": (filenames) =>
+    `next lint frontend --fix --file ${filenames
       .map((file) => file.split(process.cwd())[1])
       .join(" --file ")}`,
   "*.md": "prettier --write",
