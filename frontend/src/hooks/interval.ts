272ce04795bb29e73ea9a986d4f8fe3d226b0c23
Vincent <git@vincenttunru.com>
2022-08-30 17:03:22 +0200

    Fix parameter types and docs of useInterval

--- frontend/src/hooks/interval.ts
+++ frontend/src/hooks/interval.ts
@@ -1,8 +1,13 @@
 import { useEffect, useRef } from "react";
 
 export type IntervalFunction = () => unknown | void;
-// See https://overreacted.io/making-setinterval-declarative-with-react-hooks/
-export function useInterval(callback: IntervalFunction, delay: number) {
+/**
+ * See https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+ *
+ * @param callback Function to execute at most every `delay` milliseconds
+ * @param delay Milliseconds between calls to `callback`. `null` to disable the interval.
+ */
+export function useInterval(callback: IntervalFunction, delay: number | null) {
   const savedCallback = useRef<IntervalFunction | null>(null);
 
   // Remember the latest callback.
