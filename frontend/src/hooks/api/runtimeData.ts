c5ab9f80d49772de84c9e24b5de099752fab273d
Vincent <git@vincenttunru.com>
2022-08-30 15:37:40 +0200

    Share intro pricing end date between BE and FE

--- frontend/src/hooks/api/runtimeData.ts
+++ frontend/src/hooks/api/runtimeData.ts
@@ -16,6 +16,7 @@ export type PremiumPlans = {
 export type RuntimeData = {
   FXA_ORIGIN: string;
   GOOGLE_ANALYTICS_ID: `UA-${number}-${number}`;
+  INTRO_PRICING_END: string;
   PREMIUM_PRODUCT_ID: `prod_${string}`;
   PHONE_PRODUCT_ID: `prod_${string}`;
   PREMIUM_PLANS: PremiumPlans;
