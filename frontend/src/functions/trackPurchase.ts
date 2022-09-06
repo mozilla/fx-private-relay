951f0948916fd97112a7cd4ea659640923fa0161
Vincent <git@vincenttunru.com>
2022-09-06 18:08:52 +0200

    Count how many people complete purchases

--- frontend/src/functions/trackPurchase.ts
+++ frontend/src/functions/trackPurchase.ts
@@ -8,6 +8,8 @@ export type PurchaseTrackingArgs = Partial<ArgsWithoutCategory> &
 
 /**
  * When the user initiates the Premium purchase flow, this allows us to see whether the purchase gets completed across our different websites.
+ *
+ * @deprecated Used to track purchases when we just had a single Premium plan.
  */
 export const trackPurchaseStart = (args?: PurchaseTrackingArgs) => {
   gaEvent({
@@ -16,4 +18,53 @@ export const trackPurchaseStart = (args?: PurchaseTrackingArgs) => {
     action: args?.action ?? "Engage",
   });
   setCookie("clicked-purchase", "true", { maxAgeInSeconds: 60 * 60 });
+};
+
+export type Plan =
+  | {
+      plan: "premium";
+      billing_period: "yearly" | "monthly";
+    }
+  | {
+      plan: "phones";
+      billing_period: "yearly" | "monthly";
+    }
+  | { plan: "bundle" };
+
+export const getCookieId = (plan: Plan): string => {
+  if (plan.plan === "premium" || plan.plan === "phones") {
+    return `clicked-purchase-${plan.plan}-${plan.billing_period}`;
+  }
+  if (plan.plan === "bundle") {
+    return "clicked-purchase-bundle";
+  }
+  return null as never;
+};
+
+/**
+ * When the user initiates a plan-specific purchase flow, this allows us to see whether the purchase gets completed across our different websites.
+ */
+export const trackPlanPurchaseStart = (
+  plan: Plan,
+  args?: PurchaseTrackingArgs
+) => {
+  gaEvent({
+    ...args,
+    category: getGaCategory(plan),
+    action: args?.action ?? "Engage",
+  });
+  setCookie(getCookieId(plan), "true", { maxAgeInSeconds: 60 * 60 });
+};
+
+export const getGaCategory = (plan: Plan): string => {
+  if (plan.plan === "premium") {
+    return `Purchase ${plan.billing_period} Premium button`;
+  }
+  if (plan.plan === "phones") {
+    return `Purchase ${plan.billing_period} Premium+phones button`;
+  }
+  if (plan.plan === "bundle") {
+    return "Purchase Bundle button";
+  }
+  return null as never;
 };
