67c46629dbe707123deca7c3377a93a2407654f8
John Whitlock <jwhitlock@mozilla.com>
2022-10-11 20:01:11 -0500

    Add note for why .text
    Co-authored-by: Vincent <Vinnl@users.noreply.github.com>

--- emails/apps.py
+++ emails/apps.py
@@ -48,6 +48,7 @@ class EmailsConfig(AppConfig):
 
         # badwords file from:
         # https://www.cs.cmu.edu/~biglou/resources/bad-words.txt
+        # Using `.text` extension because of https://github.com/dependabot/dependabot-core/issues/1657
         self.badwords = self._load_terms("badwords.text")
         self.blocklist = self._load_terms("blocklist.text")
 
