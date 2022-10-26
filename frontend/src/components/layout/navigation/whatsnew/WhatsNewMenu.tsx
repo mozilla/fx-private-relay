b65c5af619efcc7882b52c0993aec7ef9993078d
Vincent <git@vincenttunru.com>
2022-10-26 14:08:50 +0200

    Turn the "News" button into icon on small screens
    Also, the pill counting how many new entries there are slightly
    overlaps the icon.

--- frontend/src/components/layout/navigation/whatsnew/WhatsNewMenu.tsx
+++ frontend/src/components/layout/navigation/whatsnew/WhatsNewMenu.tsx
@@ -64,6 +64,7 @@ import { parseDate } from "../../../../functions/parseDate";
 import { CountdownTimer } from "../../../CountdownTimer";
 import { useInterval } from "../../../../hooks/interval";
 import Link from "next/link";
+import { GiftIcon } from "../../../Icons";
 
 export type WhatsNewEntry = {
   title: string;
@@ -570,7 +571,13 @@ export const WhatsNewMenu = (props: Props) => {
           triggerState.isOpen ? styles["is-open"] : ""
         } ${props.style}`}
       >
-        {l10n.getString("whatsnew-trigger-label")}
+        <GiftIcon
+          className={styles["trigger-icon"]}
+          alt={l10n.getString("whatsnew-trigger-label")}
+        />
+        <span className={styles["trigger-label"]}>
+          {l10n.getString("whatsnew-trigger-label")}
+        </span>
         {pill}
       </button>
       {
