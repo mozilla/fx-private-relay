366962cdc47bb6eacfe651445adde5604b536c19
Vincent <git@vincenttunru.com>
2022-11-10 11:44:46 +0100

    Add a still image alternative to a tip animation

--- frontend/src/components/dashboard/tips/GenericTip.tsx
+++ frontend/src/components/dashboard/tips/GenericTip.tsx
@@ -7,6 +7,7 @@ export type GenericTipProps = {
   title: string;
   content: ReactNode;
   videos?: Record<string, string>;
+  image?: string;
 };
 // This component will probably be used for future tips that are yet to be added:
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -18,18 +19,27 @@ export const GenericTip = (props: GenericTipProps) => {
   ));
   const video =
     props.videos && getLocale(l10n).split("-")[0] === "en" ? (
-      <video
-        // The video usually uses animation to present the same information as
-        // the text below in an easier-to-understand way. Thus, there is no text
-        // alternative that wouldn't be redundant with the text below:
-        aria-hidden={true}
-        autoPlay={true}
-        loop={true}
-        muted={true}
-        playsInline={true}
-      >
-        {sources}
-      </video>
+      <>
+        <video
+          // The video usually uses animation to present the same information as
+          // the text below in an easier-to-understand way. Thus, there is no text
+          // alternative that wouldn't be redundant with the text below:
+          aria-hidden={true}
+          poster={props.image}
+          autoPlay={true}
+          loop={true}
+          muted={true}
+          playsInline={true}
+        >
+          {sources}
+          {/* Fall back to the image if the video formats are not supported: */}
+          {typeof props.image === "string" ? (
+            <img src={props.image} alt="" />
+          ) : null}
+        </video>
+        {/* This image will only be shown if the user has prefers-reduced-motion on */}
+        <img className={styles["still-alternative"]} src={props.image} alt="" />
+      </>
     ) : null;
 
   return (
