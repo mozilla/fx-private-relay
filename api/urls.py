6c14eb4230124b06b1ad0ee22cd7735e83415ec0
groovecoder <lcrouch@mozilla.com>
2022-11-11 13:57:01 -0600

    add messages endpoint

--- api/urls.py
+++ api/urls.py
@@ -71,6 +71,7 @@ urlpatterns = [
 if settings.PHONES_ENABLED:
     from .views.phones import (
         call,
+        messages,
         RealPhoneViewSet,
         RelayNumberViewSet,
         InboundContactViewSet,
@@ -108,8 +109,8 @@ if settings.PHONES_ENABLED:
             name="voice_status_deprecate_after_updating_clients",
         ),
         path("v1/voice_status/", voice_status, name="voice_status"),
-        path("v1/call", call, name="call_deprecate_after_updating_clients"),
         path("v1/call/", call, name="call"),
+        path("v1/messages/", messages, name="messages"),
         path(
             "v1/sms_status",
             sms_status,
