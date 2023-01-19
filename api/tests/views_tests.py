5d02bfdea080beceecc8e60044954515d788fc13
Vincent <git@vincenttunru.com>
2023-01-19 13:07:58 +0100

    Preserve non-traling slash Twilio webhooks
    Twilio apparently doesn't follow redirects for webhooks, so this
    allows us to migrate without breaking changes in between.
    
    This partially reverts commit
    0a497021278fd6801887dac78a7336cf641ab021.

--- api/tests/views_tests.py
+++ api/tests/views_tests.py
@@ -52,7 +52,7 @@ def test_swagger_format(client, format):
 
 @pytest.mark.parametrize("subpath", ("swagger", "swagger.", "swagger.txt"))
 def test_swagger_unknown_format(client, subpath):
-    path = f"/api/v1/{subpath}"
+    path = f"/api/v1/{subpath}/"
     response = client.get(path)
     assert response.status_code == 404
 
