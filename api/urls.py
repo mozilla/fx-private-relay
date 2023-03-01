de6dbb28215855cf7398457a2d11a063f6415023
John Whitlock <jwhitlock@mozilla.com>
2023-03-01 15:30:42 -0500

    format with black

--- api/urls.py
+++ api/urls.py
@@ -111,7 +111,7 @@ if settings.PHONES_ENABLED:
         path("v1/voice_status/", voice_status, name="voice_status"),
         path("v1/call/", call, name="call"),
         path("v1/messages/", messages, name="messages"),
-        path("v1/message/", post_message, name="post_message"),        
+        path("v1/message/", post_message, name="post_message"),
         path(
             "v1/sms_status",
             sms_status,
