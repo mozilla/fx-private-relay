d0729e0fd0d709c7dd45742422618fa3fcf31c88
mozrokafor <94625718+mozrokafor@users.noreply.github.com>
2023-03-30 11:00:39 -0400

    temp removing flaky visual regress, will re-add in a fast follow

--- e2e-tests/specs/relay-premium-upgrade.spec.ts
+++ e2e-tests/specs/relay-premium-upgrade.spec.ts
@@ -13,7 +13,7 @@ test.describe('Premium Relay - Purchase Premium Flow, Desktop', () => {
   test('Verify that the "Upgrade" button redirects correctly,  C1812640, 1808503', async ({ dashboardPage, page }) => {
     await dashboardPage.upgrade()
     expect(page.url()).toContain('/premium/')
-  })  
+  })
 })
 
 test.describe.skip(() => {
@@ -29,21 +29,21 @@ test.describe.skip(() => {
 
 test.describe.skip('Premium Relay - Purchase Premium Flow, Desktop - Visual Regression', () => {
   test.skip(({ browserName }) => browserName !== 'firefox', 'firefox only image comparisons!');
-  
+
   test.beforeEach(async ({ dashboardPage, page }) => {
     await dashboardPage.open()
     await checkAuthState(page)
   });
-  
-  test('Verify that the subscription page is displayed correctly, C1553108', async ({ subscriptionPage, dashboardPage, page }) => {    
+
+  test('Verify that the subscription page is displayed correctly, C1553108', async ({ subscriptionPage, dashboardPage, page }) => {
     await dashboardPage.upgradeNow()
     expect(page.url()).toContain('subscriptions')
 
     await expect(subscriptionPage.paypalButton).toBeVisible()
-    await expect(subscriptionPage.productDetails).toHaveScreenshot(
-        `${process.env.E2E_TEST_ENV}-productDetails.png`,
-        defaultScreenshotOpts
-    );
+    // will uncomment in a fast follow update
+    // await expect(subscriptionPage.productDetails).toHaveScreenshot(
+    //     `${process.env.E2E_TEST_ENV}-productDetails.png`,
+    //     defaultScreenshotOpts
+    // );
   })
 })
-
