250d26e8509efd825160df8882347708095bcc6d
groovecoder <lcrouch@mozilla.com>
2023-05-10 12:41:58 -0500

    fix test_record_user_signed_up_telemetry for Django 4.2

--- privaterelay/tests/signals_tests.py
+++ privaterelay/tests/signals_tests.py
@@ -2,6 +2,8 @@ from unittest.mock import patch
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.sessions.middleware import SessionMiddleware
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
 from django.test.client import RequestFactory
 from model_bakery import baker
 import pytest
@@ -22,7 +24,11 @@ def test_record_user_signed_up_telemetry():
     sign_up_request = rf.get(
         "/accounts/fxa/login/callback/?code=test&state=test&action=signin"
     )
-    middleware = SessionMiddleware()
+
+    def get_response(_: HttpRequest):
+        return HttpResponse("200 OK")
+
+    middleware = SessionMiddleware(get_response)
     middleware.process_request(sign_up_request)
     record_user_signed_up(sign_up_request, user)
 
