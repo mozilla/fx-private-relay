3edbfde0573a98c23da7411f0dc3ca22c27b6c76
Se Yeon Kim <sykim@mozilla.com>
2023-06-07 13:11:45 -0500

    Remove FxaResourceTokenAuthentication
    Not needed anymore because the FxaTokenAuthentication is enough for handling the resource provider flow

--- privaterelay/settings.py
+++ privaterelay/settings.py
@@ -842,7 +842,6 @@ if DEBUG and not IN_PYTEST:
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        # "api.authentication.FxaResourceTokenAuthentication",
         "api.authentication.FxaTokenAuthentication",
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
