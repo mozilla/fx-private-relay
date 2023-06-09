41faf853fd92ccfa114e142a41bc0fc6631f5f73
John Whitlock <jwhitlock@mozilla.com>
2023-06-09 11:25:20 -0500

    Add type stubs for django-waffle
    The main branch has types, but current release 3.0.0 does not.
    Generated with stubgen, unused files removed, and remaining files tuned
    for types.

---
+++ mypy_stubs/waffle/defaults.pyi
@@ -0,0 +1,30 @@
+# Type stubs for Django waffle
+# https://github.com/django-waffle/django-waffle/blob/v3.0.0/waffle/defaults.py
+# Can be removed once type hints ship in the release after v3.0.0
+
+COOKIE: str
+TEST_COOKIE: str
+SECURE: bool
+MAX_AGE: int
+CACHE_PREFIX: str
+CACHE_NAME: str
+FLAG_CACHE_KEY: str
+FLAG_USERS_CACHE_KEY: str
+FLAG_GROUPS_CACHE_KEY: str
+ALL_FLAGS_CACHE_KEY: str
+SAMPLE_CACHE_KEY: str
+ALL_SAMPLES_CACHE_KEY: str
+SWITCH_CACHE_KEY: str
+ALL_SWITCHES_CACHE_KEY: str
+FLAG_DEFAULT: bool
+SAMPLE_DEFAULT: bool
+SWITCH_DEFAULT: bool
+READ_FROM_WRITE_DB: bool
+CREATE_MISSING_FLAGS: bool
+CREATE_MISSING_SAMPLES: bool
+CREATE_MISSING_SWITCHES: bool
+LOG_MISSING_FLAGS: None
+LOG_MISSING_SAMPLES: None
+LOG_MISSING_SWITCHES: None
+OVERRIDE: bool
+ENABLE_ADMIN_PAGES: bool
---
+++ mypy_stubs/waffle/models.pyi
@@ -0,0 +1,135 @@
+# Type stubs for Django waffle
+# https://github.com/django-waffle/django-waffle/blob/v3.0.0/waffle/models.py
+# Can be removed once type hints ship in the release after v3.0.0
+
+from logging import Logger
+from typing import Literal, TypeVar
+
+from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
+from django.http import HttpRequest
+from django.utils.translation import gettext_lazy as _
+from django_stubs_ext.db.models import TypedModelMeta
+from waffle import managers
+
+logger: Logger
+CACHE_EMPTY: str
+
+_T = TypeVar("_T", bound="BaseModel")
+
+class BaseModel(models.Model):
+    SINGLE_CACHE_KEY: str
+    ALL_CACHE_KEY: str
+
+    class Meta(TypedModelMeta):
+        abstract = True
+    def natural_key(self) -> tuple[str, ...]: ...
+    @classmethod
+    def _cache_key(cls, name: str) -> str: ...
+    @classmethod
+    def get(cls: type[_T], name: str) -> _T: ...
+    @classmethod
+    def get_from_db(cls: type[_T], name: str) -> _T: ...
+    @classmethod
+    def get_all(cls: type[_T]) -> list[_T]: ...
+    @classmethod
+    def get_all_from_db(cls: type[_T]) -> list[_T]: ...
+    def flush(self) -> None: ...
+    def save(self, *args, **kwargs) -> None: ...
+    def delete(self, *args, **kwargs) -> tuple[int, dict[str, int]]: ...
+
+def set_flag(
+    request: HttpRequest,
+    flag_name: str,
+    active: bool = True,
+    session_only: bool = False,
+) -> None: ...
+
+class AbstractBaseFlag(BaseModel):
+    name: models.CharField
+    everyone: models.BooleanField
+    percent: models.DecimalField
+    testing: models.BooleanField
+    superusers: models.BooleanField
+    staff: models.BooleanField
+    authenticated: models.BooleanField
+    languages: models.TextField
+    rollout: models.BooleanField
+    note: models.TextField
+    created: models.DateTimeField
+    modified: models.DateTimeField
+    objects = managers.FlagManager
+    SINGLE_CACHE_KEY: str
+    ALL_CACHE_KEY: str
+
+    class Meta(TypedModelMeta):
+        abstract = True
+        verbose_name = _("Flag")
+        verbose_name_plural = _("Flags")
+    def flush(self) -> None: ...
+    def get_flush_keys(self, flush_keys: list[str] | None = None) -> list[str]: ...
+    def is_active_for_user(self, user: AbstractBaseUser) -> Literal[True] | None: ...
+    def is_active(
+        self, request: HttpRequest, read_only: bool = False
+    ) -> bool | None: ...
+
+class AbstractUserFlag(AbstractBaseFlag):
+    groups: models.ManyToManyField
+    users: models.ManyToManyField
+
+    class Meta(AbstractBaseFlag.Meta):
+        abstract = True
+        verbose_name = _("Flag")
+        verbose_name_plural = _("Flags")
+    def get_flush_keys(self, flush_keys: list[str] | None = None) -> list[str]: ...
+    def is_active_for_user(self, user: AbstractBaseUser) -> Literal[True] | None: ...
+
+class Flag(AbstractUserFlag):
+    class Meta(AbstractUserFlag.Meta):
+        swappable: str
+        verbose_name = _("Flag")
+        verbose_name_plural = _("Flags")
+
+class AbstractBaseSwitch(BaseModel):
+    name: models.CharField
+    active: models.BooleanField
+    note: models.TextField
+    created: models.DateTimeField
+    modified: models.DateTimeField
+    objects: managers.SwitchManager
+    SINGLE_CACHE_KEY: str
+    ALL_CACHE_KEY: str
+
+    class Meta(TypedModelMeta):
+        abstract = True
+        verbose_name = _("Switch")
+        verbose_name_plural = _("Switches")
+    def is_active(self) -> bool: ...
+
+class AbstractBaseSample(BaseModel):
+    name: models.CharField
+    percent: models.DecimalField
+    note: models.TextField
+    created: models.DateTimeField
+    modified: models.DateTimeField
+    objects = managers.SampleManager
+    SINGLE_CACHE_KEY: str
+    ALL_CACHE_KEY: str
+
+    class Meta(TypedModelMeta):
+        abstract = True
+        verbose_name = _("Sample")
+        verbose_name_plural = _("Samples")
+    def is_active(self) -> bool: ...
+
+class Switch(AbstractBaseSwitch):
+    class Meta(AbstractBaseSwitch.Meta):
+        swappable = "WAFFLE_SWITCH_MODEL"
+        verbose_name = _("Switch")
+        verbose_name_plural = _("Switches")
+
+class Sample(AbstractBaseSample):
+    class Meta(AbstractBaseSample.Meta):
+        swappable = "WAFFLE_SAMPLE_MODEL"
+        verbose_name = _("Sample")
+        verbose_name_plural = _("Samples")
