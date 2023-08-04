7180c97fdf465beb5d92b40c74c2527bb38a8f2c
groovecoder <lcrouch@mozilla.com>
2023-08-04 15:27:57 -0500

    MPP-3257: change send_first_email signal receiver to send_welcome_email function

--- privaterelay/signals.py
+++ privaterelay/signals.py
@@ -1,18 +1,8 @@
-from django.apps import apps
-from django.conf import settings
-from django.contrib.auth.models import User
 from django.dispatch import receiver
 
 from allauth.account.signals import user_signed_up, user_logged_in
-from django.http.request import HttpRequest
-from mypy_boto3_ses.client import SESClient
-from mypy_boto3_ses.type_defs import ContentTypeDef
-from waffle import flag_is_active
 
-from emails.apps import EmailsConfig
-from emails.utils import get_welcome_email, incr_if_enabled
-
-from .ftl_bundles import main as ftl_bundle
+from emails.utils import incr_if_enabled
 
 
 @receiver(user_signed_up)
@@ -22,36 +12,6 @@ def record_user_signed_up(request, user, **kwargs):
     # so we have to set a user_created session var for user_logged_in receiver
     request.session["user_created"] = True
     request.session.modified = True
-
-
-def _ses_message_props(data: str) -> ContentTypeDef:
-    return {"Charset": "UTF-8", "Data": data}
-
-
-@receiver(user_signed_up)
-def send_first_email(request: HttpRequest, user: User, **kwargs):
-    if not flag_is_active(request, "welcome_email"):
-        return
-    app_config = apps.get_app_config("emails")
-    assert isinstance(app_config, EmailsConfig)
-    ses_client = app_config.ses_client
-    assert ses_client
-    assert settings.RELAY_FROM_ADDRESS
-    ses_client.send_email(
-        Destination={
-            "ToAddresses": [user.email],
-        },
-        Source=settings.RELAY_FROM_ADDRESS,
-        Message={
-            "Subject": _ses_message_props(
-                ftl_bundle.format("first-time-user-email-welcome")
-            ),
-            "Body": {
-                "Html": _ses_message_props(get_welcome_email(request, user, "html")),
-                "Text": _ses_message_props(get_welcome_email(request, user, "txt")),
-            },
-        },
-    )
 
 
 @receiver(user_logged_in)
--- privaterelay/tests/signals_tests.py
+++ privaterelay/tests/signals_tests.py
@@ -1,21 +1,15 @@
 import pytest
 from unittest.mock import patch
 
-from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.test.client import RequestFactory
 
-from allauth.socialaccount.models import SocialAccount
-from django_ftl import activate
 from model_bakery import baker
-from waffle.testutils import override_flag
 
-from emails.utils import _get_hero_img_src
-from privaterelay.ftl_bundles import main as ftl_bundle
-from privaterelay.signals import record_user_signed_up, send_first_email
+from privaterelay.signals import record_user_signed_up
 
 
 @pytest.fixture()
@@ -41,56 +35,3 @@ def test_record_user_signed_up_telemetry():
 
     assert sign_up_request.session["user_created"] == True
     assert sign_up_request.session.modified == True
-
-
-@pytest.mark.django_db
-def test_record_user_signed_up_send_first_email_requires_flag(mock_ses_client):
-    test_user_email = "testuser@test.com"
-    user = baker.make(User, email=test_user_email)
-    rf = RequestFactory()
-    sign_up_request = rf.get(
-        "/accounts/fxa/login/callback/?code=test&state=test&action=signin"
-    )
-    send_first_email(sign_up_request, user)
-
-    assert not mock_ses_client.send_email.called
-
-
-@pytest.mark.django_db(transaction=True)
-@pytest.mark.parametrize("lang_code", ("en", "de", "es-es", "pt-br", "skr", "zh-tw"))
-@override_flag("welcome_email", active=True)
-def test_record_user_signed_up_send_first_email(lang_code, mock_ses_client, caplog):
-    ftl_bundle.reload()  # Reload Fluent files to regenerate errors
-    activate(lang_code)
-    test_user_email = "testuser@test.com"
-    user = baker.make(User, email=test_user_email)
-    sa = baker.make(SocialAccount, user=user)
-    sa.extra_data = {"locale": lang_code}
-    sa.save()
-    rf = RequestFactory()
-    sign_up_request = rf.get(
-        "/accounts/fxa/login/callback/?code=test&state=test&action=signin"
-    )
-    send_first_email(sign_up_request, user)
-
-    mock_ses_client.send_email.assert_called_once()
-    call_kwargs = mock_ses_client.send_email.call_args.kwargs
-    to_addresses = call_kwargs["Destination"]["ToAddresses"]
-    from_address = call_kwargs["Source"]
-    subject = call_kwargs["Message"]["Subject"]["Data"]
-    html_body = call_kwargs["Message"]["Body"]["Html"]["Data"]
-
-    assert len(to_addresses) == 1
-    assert to_addresses[0] == test_user_email
-    assert from_address == settings.RELAY_FROM_ADDRESS
-    assert subject == ftl_bundle.format("first-time-user-email-welcome")
-    assert _get_hero_img_src(lang_code) in html_body
-    assert (
-        f'href="{settings.SITE_ORIGIN}/accounts/profile/?utm_campaign=relay-onboarding&utm_source=relay-onboarding&utm_medium=email&utm_content=hero-cta"'
-        in html_body
-    )
-    assert ftl_bundle.format("first-time-user-email-hero-cta") in html_body
-
-    for log_name, log_level, message in caplog.record_tuples:
-        if log_name == "django_ftl.message_errors":
-            pytest.fail(message)
