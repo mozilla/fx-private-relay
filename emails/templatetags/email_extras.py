4c49b4848b6dc2f640b15dad9884d7a04f93043f
John Whitlock <jwhitlock@mozilla.com>
2023-09-26 11:31:20 -0500

    Wrap log lines

--- emails/templatetags/email_extras.py
+++ emails/templatetags/email_extras.py
@@ -9,16 +9,17 @@ register = template.Library()
 @register.simple_tag
 def bold_violet_text(text):
     return (
-        '<span style="font-family: sans-serif; font-weight: bolder; color: #20123a; text-decoration: none; font-size: 13px;">%s</span>'
-        % text
+        '<span style="font-family: sans-serif; font-weight: bolder; color: #20123a;'
+        f' text-decoration: none; font-size: 13px;">{text}</span>'
     )
 
 
 @register.simple_tag
 def bold_violet_link(href, link_text):
     return (
-        '<a href="%s" target="_blank" style="font-family: sans-serif; color: #20123a; text-decoration: underline; font-weight: bolder; font-size: 13px;">%s</a>'
-        % (href, link_text)
+        f'<a href="{href}" target="_blank" style="font-family: sans-serif;'
+        " color: #20123a; text-decoration: underline; font-weight: bolder;"
+        f' font-size: 13px;">{link_text}</a>'
     )
 
 
