f16ee304c34a8dc29355f8ded194f1c81c50c95a
John Whitlock <jwhitlock@mozilla.com>
2023-10-09 13:19:52 -0500

    Add test for ResponseMetrics middleware

---
+++ privaterelay/tests/middleware_tests.py
@@ -0,0 +1,22 @@
+"""Tests for privaterelay/middleware.py"""
+
+import pytest
+
+from django.test import Client
+
+from markus.testing import MetricsMock
+from pytest_django.fixtures import SettingsWrapper
+
+
+@pytest.mark.django_db
+def test_response_metrics(settings: SettingsWrapper, client: Client) -> None:
+    settings.MIDDLEWARE = ["privaterelay.middleware.ResponseMetrics"]
+    settings.DJANGO_STATSD_ENABLED = True
+
+    with MetricsMock() as mm:
+        response = client.get("/api/v1/runtime_data")
+    mm.assert_timing_once(
+        "fx.private.relay.response",
+        tags=["status:200", "view:api.views.runtime_data", "method:GET"],
+    )
+    assert response.status_code == 200
