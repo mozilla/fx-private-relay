272662e813be8054f7f98482ece48f3046238154
John Whitlock <jwhitlock@mozilla.com>
2023-11-16 09:59:06 -0600

    Fix CSS sourceMappingURL, add cache invalidation
    Django 4.1 started "fixing" the CSS sourceMappingURL comment, which
    added an additional space to next.jss CSS files. This meant the content
    changed without the name changing, causing issues when caches returned
    old version of the file.
    
    This leans into that change, further fixing the URL so that it is
    accurate when next.js injects the content as an inline style.
    
    It also adds a short comment to globals.scss and tokens.scss that Sass
    will preserve in the output CSS. This allows changing the content of the
    CSS, so next.js will change the CSS filenames, and the cache will be
    invaldiated. This should only be necessary around Django updates
    for django.contrib.staticfiles.

--- privaterelay/storage.py
+++ privaterelay/storage.py
@@ -3,6 +3,8 @@ Staticfiles storage implementation for Relay.
 
 This is used when running ./manage.py collectstatic, or rendering Django templates.
 """
+from typing import Any
+
 from whitenoise.storage import CompressedManifestStaticFilesStorage
 
 
@@ -10,24 +12,47 @@ class RelayStaticFilesStorage(CompressedManifestStaticFilesStorage):
     """
     Customize Whitenoise storage for Relay
 
-    The Django ManifestStaticFilesStorage [1] creates a copy of each file that
+    The Django ManifestStaticFilesStorage creates a copy of each file that
     includes a hash of its contents, and serves these with a long cache time.
     It also creates staticfiles/staticfiles.json that lists all the known static
     files, rather than scanning the folder for files at startup.
 
-    The Whitenoise CompressedManifestStaticFilesStorage [2] builds on this by
+    The Whitenoise CompressedManifestStaticFilesStorage builds on this by
     pre-compressing files as well, so that the gzipped versions can be served.
 
     This class skips renaming files from Next.js, which already include hashes
     in the filenames.
 
-    [1] https://docs.djangoproject.com/en/3.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
-    [2] http://whitenoise.evans.io/en/stable/django.html#add-compression-and-caching-support
+    See:
+    https://docs.djangoproject.com/en/4.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
+    http://whitenoise.evans.io/en/stable/django.html#add-compression-and-caching-support
     """
 
-    def hashed_name(self, name, content=None, filename=None):
-        """Skip hashing files output by Next.js"""
+    def hashed_name(
+        self, name: str, content: str | None = None, filename: str | None = None
+    ) -> str:
+        """Skip hashing filenames output by Next.js"""
         if name.startswith("_next/static/"):
             return name
         else:
-            return super().hashed_name(name, content, filename)
+            new_name = super().hashed_name(name, content, filename)
+            assert isinstance(new_name, str)
+            return new_name
+
+    def url_converter(
+        self, name: str, hashed_files: dict[str, str], template: str | None = None
+    ) -> Any:
+        """
+        Convert Next.js source map URL to absolute URL.
+
+        If this changes, or other django.contrib.staticfiles changes adjust CSS output,
+        then update the cache version in globals.scss and tokens.scss to bust the cache.
+        """
+        if (
+            name.startswith("_next/static/css/")
+            and name.endswith(".css")
+            and template == "/*# sourceMappingURL=%(url)s */"
+        ):
+            path = name.rsplit("/", 1)[0]
+            template = f"/*# sourceMappingURL={self.base_url}{path}/%(url)s */"
+        return super().url_converter(name, hashed_files, template)
