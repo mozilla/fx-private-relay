16083be169b7993ed4a744077d9e4a04ce511b41
lloan alas <lloanalas@outlook.com>
2023-11-21 05:52:21 -0800

    fix formatting issue

--- frontend/src/components/layout/navigation/whatsnew/WhatsNewMenu.tsx
+++ frontend/src/components/layout/navigation/whatsnew/WhatsNewMenu.tsx
@@ -39,8 +39,8 @@ import PremiumFinlandHero from "./images/premium-expansion-finland-hero.svg";
 import PremiumFinlandIcon from "./images/premium-expansion-finland-icon.svg";
 import PhoneMaskingHero from "./images/phone-masking-hero.svg";
 import PhoneMaskingIcon from "./images/phone-masking-icon.svg";
-import HolidayPromo2023Icon from "./images/holiday-promo-2023-news-icon.svg"; 
-import HolidayPromo2023Hero from "./images/holiday-promo-2023-news-hero.svg";   
+import HolidayPromo2023Icon from "./images/holiday-promo-2023-news-icon.svg";
+import HolidayPromo2023Hero from "./images/holiday-promo-2023-news-hero.svg";
 import BundleHero from "./images/bundle-promo-hero.svg";
 import BundleIcon from "./images/bundle-promo-icon.svg";
 import OfferCountdownIcon from "./images/offer-countdown-icon.svg";
@@ -337,13 +337,13 @@ export const WhatsNewMenu = (props: Props) => {
   ) {
     entries.push(premiumInFinland);
   }
- 
+
   const yearlyPlanLink = getPeriodicalPremiumSubscribeLink(
     props.runtimeData as RuntimeDataWithPeriodicalPremiumAvailable,
     "yearly",
   );
   const yearlyPlanRefWithCoupon = `${yearlyPlanLink}&coupon=HOLIDAY2023`;
-   
+
   const holidayPromo2023: WhatsNewEntry = {
     title: l10n.getString("whatsnew-holiday-promo-2023-news-heading"),
     snippet: l10n.getString("whatsnew-holiday-promo-2023-news-snippet"),
@@ -352,9 +352,7 @@ export const WhatsNewMenu = (props: Props) => {
         description={l10n.getString(
           "whatsnew-holiday-promo-2023-news-content-description",
         )}
-        heading={l10n.getString(
-          "whatsnew-holiday-promo-2023-news-heading",
-        )}
+        heading={l10n.getString("whatsnew-holiday-promo-2023-news-heading")}
         image={HolidayPromo2023Hero}
         cta={
           <Link href={yearlyPlanRefWithCoupon} legacyBehavior>
