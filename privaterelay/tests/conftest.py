8bcb2184476acb9ed0b1472d5de642d802081c2c
John Whitlock <jwhitlock@mozilla.com>
2024-01-17 13:44:07 -0600

    Add glean_logger(), fix document_id

--- privaterelay/tests/conftest.py
+++ privaterelay/tests/conftest.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 from typing import Iterator
+import json
 
 import pytest
 
@@ -14,5 +15,12 @@ def version_json_path(tmp_path, settings) -> Iterator[Path]:
     get_version_info.cache_clear()
     settings.BASE_DIR = tmp_path
     path = settings.BASE_DIR / "version.json"
+    default_build_info = {
+        "commit": "the_commit_hash",
+        "version": "2024.01.17",
+        "source": "https://github.com/mozilla/fx-private-relay",
+        "build": "https://circleci.com/gh/mozilla/fx-private-relay/100",
+    }
+    path.write_text(json.dumps(default_build_info))
     yield path
     get_version_info.cache_clear()
