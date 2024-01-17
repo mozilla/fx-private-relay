4748713dc60014146fae2dc99b56834dc859fa74
John Whitlock <jwhitlock@mozilla.com>
2024-01-17 09:49:53 -0600

    Refactor to get_version_info

--- privaterelay/settings.py
+++ privaterelay/settings.py
@@ -46,7 +46,7 @@ except ImportError:
     HAS_SILK = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TMP_DIR = os.path.join(BASE_DIR, "tmp")
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
