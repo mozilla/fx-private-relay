74fd2b67686a8627c62d7c68bc9b9e2bd2647429
John Whitlock <jwhitlock@mozilla.com>
2024-01-18 11:58:40 -0600

    Fix DomainAddress.perform_create, test conflicts

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -28,6 +28,7 @@ from rest_framework.exceptions import (
     ParseError,
 )
 from rest_framework.response import Response
+from rest_framework.serializers import BaseSerializer
 from rest_framework.views import exception_handler
 
 from allauth.account.adapter import get_adapter as get_account_adapter
@@ -63,7 +64,7 @@ from emails.models import (
 )
 
 from ..authentication import get_fxa_uid_from_oauth_token
-from ..exceptions import ConflictError, RelayAPIException
+from ..exceptions import DomainAddressConflictError, RelayAPIException
 from ..permissions import IsOwner, CanManageFlags
 from ..serializers import (
     DomainAddressSerializer,
@@ -153,16 +154,19 @@ class DomainAddressViewSet(SaveToRequestUser, viewsets.ModelViewSet):
     def get_queryset(self):
         return DomainAddress.objects.filter(user=self.request.user)
 
-    def perform_create(self, serializer):
+    def perform_create(self, serializer: BaseSerializer[DomainAddress]) -> None:
         try:
             serializer.save(user=self.request.user)
         except IntegrityError:
+            assert isinstance(self.request.user, User)
             domain_address = DomainAddress.objects.filter(
                 user=self.request.user, address=serializer.validated_data.get("address")
             ).first()
-            raise ConflictError(
-                {"id": domain_address.id, "full_address": domain_address.full_address}
-            )
+            existing_id = existing_address = None
+            if domain_address:
+                existing_id = domain_address.id
+                existing_address = domain_address.full_address
+            raise DomainAddressConflictError(existing_id, existing_address)
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
