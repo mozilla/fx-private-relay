2ef2cfeeeed0769e6e5757d3f14d384b1f60efc4
Sascha Ronnie Daoudia <85792632+Dadudidas@users.noreply.github.com>
2024-01-27 16:33:45 +0100

    Dadudida  fx-private-relay (#3)
    * Fix selectors for account signup
    
    Mozilla accounts has change their input field identifiers on stage,
    including submit buttons with no common identifier.
    
    * Use a regex for the return from premium signup
    
    The previous URL match had trouble with an empty querystring, like
    /accounts/profile/?
    
    * Add newline at end of file
    
    * for MPP-3467: add googlecloudprofiler to PrivateRelayConfig.ready
    
    Also moves get_version_info to privaterelay.apps
    
    ---------
    
    Co-authored-by: John Whitlock <jwhitlock@mozilla.com>
    Co-authored-by: groovecoder <lcrouch@mozilla.com>
    Co-authored-by: luke crouch <luke.crouch@gmail.com>

--- e2e-tests/pages/authPage.ts
+++ e2e-tests/pages/authPage.ts
@@ -5,27 +5,31 @@ export class AuthPage {
     readonly emailInputField: Locator
     readonly passwordInputField: Locator
     readonly passwordConfirmInputField: Locator
-    readonly continueButton: Locator
     readonly ageInputField: Locator
+    readonly continueButton: Locator
+    readonly createAccountButton: Locator
     readonly verifyCodeInputField: Locator
+    readonly confirmCodeButton: Locator
 
     constructor(page: Page){
         this.page = page;
         this.emailInputField = page.locator('input[name="email"]');
-        this.passwordInputField = page.locator('#password');        
-        this.passwordConfirmInputField = page.locator('#vpassword');
-        this.ageInputField = page.locator('#age');
+        this.passwordInputField = page.getByTestId('new-password-input-field');
+        this.passwordConfirmInputField = page.getByTestId('verify-password-input-field');
+        this.ageInputField = page.getByTestId('age-input-field');
         this.continueButton = page.locator('#submit-btn');
-        this.verifyCodeInputField = page.locator('div.card input')
+        this.createAccountButton = page.getByRole('button', { name: 'Create account' });
+        this.verifyCodeInputField = page.getByTestId('confirm-signup-code-input-field');
+        this.confirmCodeButton = page.getByRole('button', { name: 'Confirm' });
     }
 
     async continue() {
-        await this.continueButton.click()
+        await this.continueButton.click();
     }
 
     async enterVerificationCode(code: string){
-        await this.verifyCodeInputField.fill(code)
-        await this.continue()
+        await this.verifyCodeInputField.fill(code);
+        await this.confirmCodeButton.click();
     }
 
     async enterEmail(email: string) {
@@ -37,7 +41,7 @@ export class AuthPage {
         await this.passwordInputField.fill(process.env.E2E_TEST_ACCOUNT_PASSWORD as string);
         await this.continue();
     }
-    
+
     async login(email: string) {
         await this.enterEmail(email);
         await this.enterPassword();
@@ -48,6 +52,6 @@ export class AuthPage {
         await this.passwordInputField.fill(process.env.E2E_TEST_ACCOUNT_PASSWORD as string);
         await this.passwordConfirmInputField.fill(process.env.E2E_TEST_ACCOUNT_PASSWORD as string);
         await this.ageInputField.type("31");
-        await this.continue();
+        await this.createAccountButton.click()
     }
 }
--- privaterelay/apps.py
+++ privaterelay/apps.py
@@ -1,3 +1,5 @@
+import json
+from pathlib import Path
 from typing import Any
 import requests
 import os
@@ -10,10 +12,48 @@ from django.utils.functional import cached_property
 ROOT_DIR = os.path.abspath(os.curdir)
 
 
+def get_profiler_startup_data() -> tuple[str | None, str | None]:
+    from .utils import get_version_info
+
+    if settings.RELAY_CHANNEL not in ("dev", "stage", "prod"):
+        return (None, None)
+
+    if settings.RELAY_CHANNEL in ("dev", "stage"):
+        service = f"fxprivaterelay-{settings.RELAY_CHANNEL}"
+    if settings.RELAY_CHANNEL == "prod":
+        service = "fxprivaterelay"
+
+    version_info = get_version_info()
+    version = version_info.get("version", "unknown")
+
+    return service, version
+
+
 class PrivateRelayConfig(AppConfig):
     name = "privaterelay"
 
     def ready(self) -> None:
+        if settings.GOOGLE_APPLICATION_CREDENTIALS is not None:
+            # Set up Google Cloud Profiler
+            service, version = get_profiler_startup_data()
+            if service != None:
+                import googlecloudprofiler
+
+                # Make sure the expect gcp_key.json file exists
+                gcp_key_json_path = Path(settings.GOOGLE_APPLICATION_CREDENTIALS)
+                if gcp_key_json_path.exists():
+                    with gcp_key_json_path.open() as gcp_key_file:
+                        try:
+                            # Make sure the expect gcp_key.json file is valid json
+                            gcp_key_data = json.load(gcp_key_file)
+                            googlecloudprofiler.start(
+                                service=service,
+                                service_version=version,
+                                project_id=gcp_key_data["project_id"],
+                            )
+                        except ValueError:
+                            print(f"error during json.load({gcp_key_json_path})")
+
         import privaterelay.signals
 
         assert privaterelay.signals  # Suppress "imported but unused" warnings
