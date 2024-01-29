fbc255351386dace5303420f0dea162fb759e9ae
groovecoder <lcrouch@mozilla.com>
2024-01-29 14:02:56 -0600

    create Google app creds file from env var as needed

--- .profile
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Heroku-only file to copy env var credentials to gcp_key.json file
-echo "${GOOGLE_CREDENTIALS_B64}" | base64 -d > gcp_key.json
--- privaterelay/apps.py
+++ privaterelay/apps.py
@@ -1,3 +1,4 @@
+import base64
 import json
 from pathlib import Path
 from typing import Any
@@ -29,30 +30,48 @@ def get_profiler_startup_data() -> tuple[str | None, str | None]:
     return service, version
 
 
+def write_gcp_key_json_file(gcp_key_json_path: Path) -> None:
+    # create the gcp key json file from contents of GOOGLE_CLOUD_PROFILER_CREDENTIALS_B64
+    google_app_creds = base64.b64decode(settings.GOOGLE_CLOUD_PROFILER_CREDENTIALS_B64)
+    if not google_app_creds == b"":
+        with open(gcp_key_json_path, "w+") as gcp_key_file:
+            gcp_key_file.write(google_app_creds.decode("utf-8"))
+
+
 class PrivateRelayConfig(AppConfig):
     name = "privaterelay"
 
     def ready(self) -> None:
-        if settings.GOOGLE_APPLICATION_CREDENTIALS:
+        if (
+            settings.GOOGLE_APPLICATION_CREDENTIALS != ""
+            and settings.GOOGLE_CLOUD_PROFILER_CREDENTIALS_B64 != ""
+        ):
             # Set up Google Cloud Profiler
             service, version = get_profiler_startup_data()
             if service != None:
-                import googlecloudprofiler
-
-                # Make sure the expect gcp_key.json file exists
                 gcp_key_json_path = Path(settings.GOOGLE_APPLICATION_CREDENTIALS)
-                if gcp_key_json_path.exists() and gcp_key_json_path.is_file():
+                if not gcp_key_json_path.exists():
+                    write_gcp_key_json_file(gcp_key_json_path)
+                try:
                     with gcp_key_json_path.open() as gcp_key_file:
                         try:
                             # Make sure the expect gcp_key.json file is valid json
                             gcp_key_data = json.load(gcp_key_file)
+                            import googlecloudprofiler
+
                             googlecloudprofiler.start(
                                 service=service,
                                 service_version=version,
                                 project_id=gcp_key_data["project_id"],
                             )
-                        except ValueError:
+                        except json.JSONDecodeError:
                             print(f"error during json.load({gcp_key_json_path})")
+                except Exception as exc:
+                    print(
+                        f"exception {repr(exc)}"
+                        " while starting google cloud profiler"
+                        f" with key file: {gcp_key_json_path}"
+                    )
 
         import privaterelay.signals
 
