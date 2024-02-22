d02e01383b1d01a5a0c687a69f4c49de098d0528
John Whitlock <jwhitlock@mozilla.com>
2024-02-22 12:48:21 -0600

    Add annotations to log_record test utility

--- privaterelay/tests/utils.py
+++ privaterelay/tests/utils.py
@@ -1,4 +1,10 @@
-def log_extra(log_record):
+"""Helper functions for tests"""
+
+from logging import LogRecord
+from typing import Any
+
+
+def log_extra(log_record: LogRecord) -> dict[str, Any]:
     """Reconstruct the "extra" argument to the log call"""
     omit_log_record_keys = set(
         (
