f2e4f970ae1c55668d4e28cad258cab1b408db57
John Whitlock <jwhitlock@mozilla.com>
2024-03-26 16:40:03 -0500

    Remove TODO testcase for MPP-3753
    This was handled in PR #4519 by making it an error.

--- api/tests/views_tests.py
+++ api/tests/views_tests.py
@@ -506,8 +506,6 @@ def test_patch_domainaddress_same_value_used_on(
     "key,value",
     [
         ("id", -1),
-        # TODO MPP-3753: Make address a write-once field
-        pytest.param("address", "a-different-alias", marks=pytest.mark.xfail),
         ("domain", 1),
         ("full_address", "a-different-alias@premium.test.com"),
         ("created_at", "2024-02-15"),
