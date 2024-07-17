919c2fef764a1890f12e6cbd1f4c8739929cb41c
John Whitlock <jwhitlock@mozilla.com>
2024-07-17 12:54:19 -0500

    Add types, fix code in signals
    These changes were made in previous PRs but missed in this PR.

--- privaterelay/signals.py
+++ privaterelay/signals.py
@@ -1,9 +1,11 @@
 import logging
 from hashlib import sha256
+from typing import Any
 
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+from django.http import HttpRequest
 
 from allauth.account.signals import user_logged_in, user_signed_up
 from rest_framework.authtoken.models import Token
@@ -16,7 +18,7 @@ info_logger = logging.getLogger("eventsinfo")
 
 
 @receiver(user_signed_up, dispatch_uid="privaterelay_record_user_signed_up")
-def record_user_signed_up(request, user, **kwargs):
+def record_user_signed_up(request: HttpRequest, user: User, **kwargs: Any) -> None:
     incr_if_enabled("user_signed_up", 1)
     # the user_signed_up signal doesn't have access to the response object
     # so we have to set a user_created session var for user_logged_in receiver
@@ -25,7 +27,7 @@ def record_user_signed_up(request, user, **kwargs):
 
 
 @receiver(user_logged_in, dispatch_uid="privaterelay_record_user_logged_in")
-def record_user_logged_in(request, user, **kwargs):
+def record_user_logged_in(request: HttpRequest, user: User, **kwargs: Any) -> None:
     incr_if_enabled("user_logged_in", 1)
     response = kwargs.get("response")
     event = "user_logged_in"
@@ -38,14 +40,18 @@ def record_user_logged_in(request, user, **kwargs):
 
 
 @receiver(post_save, sender=User, dispatch_uid="privaterelay_create_user_profile")
-def create_user_profile(sender, instance, created, **kwargs):
+def create_user_profile(
+    sender: type[User], instance: User, created: bool, **kwargs: Any
+) -> None:
     if created:
         set_user_group(instance)
         Profile.objects.create(user=instance)
 
 
 @receiver(pre_save, sender=Profile, dispatch_uid="measure_feature_usage")
-def measure_feature_usage(sender, instance, **kwargs):
+def measure_feature_usage(
+    sender: type[Profile], instance: Profile, **kwargs: Any
+) -> None:
     if instance._state.adding:
         # if newly created Profile ignore the signal
         return
@@ -61,19 +67,28 @@ def measure_feature_usage(sender, instance, **kwargs):
             incr_if_enabled("tracker_removal_enabled")
         if not instance.remove_level_one_email_trackers:
             incr_if_enabled("tracker_removal_disabled")
+        if instance.fxa:
+            # TODO create a utility function or property for hashed fxa uid
+            hashed_uid = sha256(instance.fxa.uid.encode("utf-8")).hexdigest()
+        else:
+            hashed_uid = "_no_fxa_"
         info_logger.info(
             "tracker_removal_feature",
             extra={
                 "enabled": instance.remove_level_one_email_trackers,
-                # TODO create a utility function or property for hashed fxa uid
-                "hashed_uid": sha256(instance.fxa.uid.encode("utf-8")).hexdigest(),
+                "hashed_uid": hashed_uid,
             },
         )
 
 
 @receiver(post_save, sender=Profile, dispatch_uid="privaterelay_copy_auth_token")
-def copy_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
+def copy_auth_token(
+    sender: type[Profile],
+    instance: Profile | None = None,
+    created: bool = False,
+    **kwargs: Any,
+) -> None:
+    if created and instance is not None:
         # baker triggers created during tests
         # so first check the user doesn't already have a Token
         try:
