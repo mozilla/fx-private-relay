3b451903f1817436c1943b8dc512debd774dc77c
John Whitlock <jwhitlock@mozilla.com>
2024-12-02 11:54:18 -0600

    Refactor to cached_signing_key

--- emails/tests/sns_tests.py
+++ emails/tests/sns_tests.py
@@ -21,6 +21,11 @@ from ..sns import NOTIFICATION_HASH_FORMAT, _grab_keyfile, verify_from_sns
 
 @pytest.fixture(autouse=True)
 def key_cache(settings: SettingsWrapper) -> Iterator[BaseCache]:
+    """
+    Return the cache used for signing certificates.
+
+    Clear the cache before and after tests.
+    """
     key_cache = caches[getattr(settings, "AWS_SNS_KEY_CACHE", "default")]
     key_cache.clear()
     yield key_cache
@@ -29,6 +34,7 @@ def key_cache(settings: SettingsWrapper) -> Iterator[BaseCache]:
 
 @pytest.fixture
 def key_and_cert() -> tuple[rsa.RSAPrivateKey, x509.Certificate]:
+    """Generate an RSA key and a signing certificate"""
     key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
     name_attributes = [
         x509.NameAttribute(NameOID.COUNTRY_NAME, "US"),
@@ -49,6 +55,20 @@ def key_and_cert() -> tuple[rsa.RSAPrivateKey, x509.Certificate]:
         .sign(key, hashes.SHA256())
     )
     return key, cert
+
+
+@pytest.fixture
+def cached_signing_key(
+    key_and_cert: tuple[rsa.RSAPrivateKey, x509.Certificate],
+    key_cache: BaseCache,
+    settings: SettingsWrapper,
+) -> tuple[str, rsa.RSAPrivateKey]:
+    """Store the signing certificate in the cache."""
+    cert_url = f"https://sns.{settings.AWS_REGION}.amazonaws.com/cert.pem"
+    key, cert = key_and_cert
+    cert_pem = cert.public_bytes(serialization.Encoding.PEM)
+    key_cache.set(cert_url, cert_pem)
+    return cert_url, key
 
 
 @pytest.fixture
@@ -109,15 +129,9 @@ def test_grab_keyfile_cert_chain_fails(
 
 
 def test_verify_from_sns_notification_with_subject_ver1(
-    key_and_cert: tuple[rsa.RSAPrivateKey, x509.Certificate],
-    key_cache: BaseCache,
-    settings: SettingsWrapper,
+    cached_signing_key: tuple[str, rsa.RSAPrivateKey],
 ) -> None:
-    cert_url = f"https://sns.{settings.AWS_REGION}.amazonaws.com/cert.pem"
-    key, cert = key_and_cert
-    cert_pem = cert.public_bytes(serialization.Encoding.PEM)
-    key_cache.set(cert_url, cert_pem)
-
+    cert_url, key = cached_signing_key
     json_body = {
         "Type": "Notification",
         "Message": "message",
