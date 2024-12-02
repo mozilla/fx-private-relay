fcc6026084112234023501dc9f132ec724c3c86e
John Whitlock <jwhitlock@mozilla.com>
2024-12-02 10:27:07 -0600

    Convert to pytest functions, fixtures

--- emails/tests/sns_tests.py
+++ emails/tests/sns_tests.py
@@ -1,15 +1,21 @@
-from unittest.mock import patch
+from collections.abc import Iterator
+from unittest.mock import Mock, patch
 
 from django.core.exceptions import SuspiciousOperation
-from django.test import TestCase
+
+import pytest
 
 from ..sns import _grab_keyfile
 
 
-class GrabKeyfileTest(TestCase):
-    @patch("emails.sns.urlopen")
-    def test_grab_keyfile_checks_cert_url_origin(self, mock_urlopen):
-        cert_url = "https://attacker.com/cert.pem"
-        with self.assertRaises(SuspiciousOperation):
-            _grab_keyfile(cert_url)
-        mock_urlopen.assert_not_called()
+@pytest.fixture
+def mock_urlopen() -> Iterator[Mock]:
+    with patch("emails.sns.urlopen") as mock_urlopen:
+        yield mock_urlopen
+
+
+def test_grab_keyfile_checks_cert_url_origin(mock_urlopen: Mock) -> None:
+    cert_url = "https://attacker.com/cert.pem"
+    with pytest.raises(SuspiciousOperation):
+        _grab_keyfile(cert_url)
+    mock_urlopen.assert_not_called()
