5724b0759fa2f0438030be8986268561518f29a3
John Whitlock <jwhitlock@mozilla.com>
2024-12-19 10:24:38 -0600

    Use setup_fxa_introspect for all introspect tests

--- api/tests/authentication_tests.py
+++ api/tests/authentication_tests.py
@@ -1,4 +1,3 @@
-import json
 import re
 from datetime import datetime
 from typing import Any
@@ -299,7 +298,7 @@ class IntrospectTokenTests(TestCase):
 
     @responses.activate
     def test_timeout_returns_error(self):
-        mock_response = _mock_fxa_introspect_response(timeout=True)
+        mock_response, fxa_data = setup_fxa_introspect(timeout=True)
 
         fxa_resp = introspect_token("the-token")
         assert fxa_resp == IntrospectionError("Timeout")
@@ -307,7 +306,7 @@ class IntrospectTokenTests(TestCase):
 
     @responses.activate
     def test_other_request_exception_returns_error(self):
-        mock_response = _mock_fxa_introspect_response(
+        mock_response, fxa_data = setup_fxa_introspect(
             exception=Exception("An Exception")
         )
 
@@ -327,14 +326,11 @@ class IntrospectTokenTests(TestCase):
 
     @responses.activate
     def test_list_body_returns_error(self) -> None:
-        valid_data = _create_fxa_introspect_response()
-        invalid_data = [valid_data]
-        invalid_text = json.dumps(invalid_data)
-        mock_response, _ = setup_fxa_introspect(text_body=invalid_text)
+        mock_response, _ = setup_fxa_introspect(text_body='[{"active": false}]')
 
         fxa_resp = introspect_token("the-token")
         assert fxa_resp == IntrospectionError(
-            "NotJsonDict", [invalid_text], status_code=200
+            "NotJsonDict", ['[{"active": false}]'], status_code=200
         )
         assert mock_response.call_count == 1
 
