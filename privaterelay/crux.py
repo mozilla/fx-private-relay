0e5df0d36b042c6d5696244d32ae23908bf0de63
John Whitlock <jwhitlock@mozilla.com>
2025-01-28 16:41:38 -0600

    Add CruxQuery

--- privaterelay/crux.py
+++ privaterelay/crux.py
@@ -4,7 +4,7 @@ Interface to Chrome User Experience (CrUX) API
 https://developer.chrome.com/docs/crux/api
 """
 
-from typing import Any
+from typing import Any, Literal
 
 
 def main(crux_api_requester: Any) -> str:
@@ -12,6 +12,47 @@ def main(crux_api_requester: Any) -> str:
     results = gather_api_results(qp, crux_api_requester)
     report = create_command_line_report(results)
     return report
+
+
+CRUX_FORM_FACTOR = Literal["PHONE", "TABLET", "DESKTOP"]
+CRUX_METRIC = Literal[
+    "cumulative_layout_shift",
+    "first_contentful_paint",
+    "interaction_to_next_paint",
+    "largest_contentful_paint",
+    "experimental_time_to_first_byte",
+    "navigation_types",
+    "form_factors",
+    "round_trip_time",
+]
+
+
+class CruxQuery:
+    def __init__(
+        self,
+        origin: str,
+        form_factor: CRUX_FORM_FACTOR | None = None,
+        metrics: list[CRUX_METRIC] | None = None,
+    ) -> None:
+        self.origin = origin
+        self.form_factor = form_factor
+        self.metrics = metrics
+
+    def __repr__(self) -> str:
+        args = [repr(self.origin)]
+        if self.form_factor is not None:
+            args.append(f"form_factor={self.form_factor!r}")
+        if self.metrics is not None:
+            args.append(f"metrics={sorted(self.metrics)!r}")
+        return f"{self.__class__.__name__}({', '.join(args)})"
+
+    def as_dict(self) -> dict[str, Any]:
+        result: dict[str, Any] = {"origin": self.origin}
+        if self.form_factor is not None:
+            result["form_factor"] = self.form_factor
+        if self.metrics is not None:
+            result["metrics"] = sorted(self.metrics)
+        return result
 
 
 def get_main_query_parameters() -> Any:
--- privaterelay/tests/crux_tests.py
+++ privaterelay/tests/crux_tests.py
@@ -1,6 +1,33 @@
 """Tests for privaterelay/crux.py"""
 
-from ..crux import main
+from ..crux import CruxQuery, main
+
+
+def test_crux_query_only_origin() -> None:
+    query = CruxQuery("https://example.com")
+    assert query.as_dict() == {"origin": "https://example.com"}
+    assert repr(query) == "CruxQuery('https://example.com')"
+
+
+def test_crux_query_form_factor() -> None:
+    query = CruxQuery("https://example.com", form_factor="PHONE")
+    assert query.as_dict() == {"origin": "https://example.com", "form_factor": "PHONE"}
+    assert repr(query) == "CruxQuery('https://example.com', form_factor='PHONE')"
+
+
+def test_crux_query_metric() -> None:
+    query = CruxQuery(
+        "https://example.com",
+        metrics=["largest_contentful_paint", "first_contentful_paint"],
+    )
+    assert query.as_dict() == {
+        "origin": "https://example.com",
+        "metrics": ["first_contentful_paint", "largest_contentful_paint"],
+    }
+    assert repr(query) == (
+        "CruxQuery('https://example.com',"
+        " metrics=['first_contentful_paint', 'largest_contentful_paint'])"
+    )
 
 
 def test_main() -> None:
