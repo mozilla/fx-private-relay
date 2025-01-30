2cf9dafb6b0c4aa08680511cb5626aaf1792cba1
John Whitlock <jwhitlock@mozilla.com>
2025-01-30 09:47:26 -0600

    Add realistic responses for tests

--- privaterelay/tests/crux_tests.py
+++ privaterelay/tests/crux_tests.py
@@ -203,8 +203,106 @@ def test_stubbed_engine_unexpected_request() -> None:
     assert engine.requests == [expected_request]
 
 
-def test_main() -> None:
+def test_crux_api_requester_success() -> None:
+    query = CruxQuery("https://example.com", metrics=["cumulative_layout_shift"])
+    expected_request = StubbedRequest(
+        url=CruxApiRequester.API_URL,
+        params={"key": "API_KEY"},
+        data={"origin": "https://example.com", "metrics": ["cumulative_layout_shift"]},
+        timeout=CruxApiRequester.DEFAULT_TIMEOUT,
+    )
+    record = {
+        "record": {
+            "key": {"origin": "https://relay.firefox.com"},
+            "metrics": {
+                "cumulative_layout_shift": {
+                    "histogram": [
+                        {"start": "0.00", "end": "0.10", "density": 0.8077},
+                        {"start": "0.10", "end": "0.25", "density": 0.1003},
+                        {"start": "0.25", "density": 0.092},
+                    ],
+                    "percentiles": {"p75": "0.07"},
+                },
+            },
+            "collectionPeriod": {
+                "firstDate": {"year": 2024, "month": 12, "day": 31},
+                "lastDate": {"year": 2025, "month": 1, "day": 27},
+            },
+        }
+    }
+    action = StubbedRequestAction(200, record)
     engine = StubbedEngine()
+    engine.expect_request(expected_request, action)
+    requester = CruxApiRequester("API_KEY", engine)
+    status_code, data = requester.query(query)
+    assert status_code == 200
+    assert data == record  # TODO: parsed record
+
+
+def test_crux_api_requester_permission_denied() -> None:
+    query = CruxQuery("https://example.com", metrics=["cumulative_layout_shift"])
+    expected_request = StubbedRequest(
+        url=CruxApiRequester.API_URL,
+        params={"key": "API_KEY"},
+        data={"origin": "https://example.com", "metrics": ["cumulative_layout_shift"]},
+        timeout=CruxApiRequester.DEFAULT_TIMEOUT,
+    )
+    proj_num = "1234567890"
+    enable_url = (
+        "https://console.developers.google.com/apis/api/chromeuxreport.googleapis.com"
+        f"/overview?project={proj_num}"
+    )
+    err_msg = (
+        f"Chrome UX Report API has not been used in project {proj_num} before or it"
+        f" is disabled. Enable it by visiting {enable_url} then retry. If you enabled"
+        " this API recently, wait a few minutes for the action to propagate to our"
+        " systems and retry."
+    )
+    error = {
+        "error": {
+            "code": 403,
+            "message": err_msg,
+            "status": "PERMISSION_DENIED",
+            "details": [
+                {
+                    "@type": "type.googleapis.com/google.rpc.ErrorInfo",
+                    "reason": "SERVICE_DISABLED",
+                    "domain": "googleapis.com",
+                    "metadata": {
+                        "containerInfo": proj_num,
+                        "activationUrl": enable_url,
+                        "serviceTitle": "Chrome UX Report API",
+                        "consumer": f"projects/{proj_num}",
+                        "service": "chromeuxreport.googleapis.com",
+                    },
+                },
+                {
+                    "@type": "type.googleapis.com/google.rpc.LocalizedMessage",
+                    "locale": "en-US",
+                    "message": err_msg,
+                },
+                {
+                    "@type": "type.googleapis.com/google.rpc.Help",
+                    "links": [
+                        {
+                            "description": "Google developers console API activation",
+                            "url": enable_url,
+                        }
+                    ],
+                },
+            ],
+        }
+    }
+    action = StubbedRequestAction(403, error)
+    engine = StubbedEngine()
+    engine.expect_request(expected_request, action)
+    requester = CruxApiRequester("API_KEY", engine)
+    status_code, data = requester.query(query)
+    assert status_code == 403
+    assert data == error  # TODO: parsed error
+
+
+def test_main() -> None:
     expected_request = StubbedRequest(
         url=CruxApiRequester.API_URL,
         params={"key": "API_KEY"},
@@ -212,6 +310,7 @@ def test_main() -> None:
         timeout=CruxApiRequester.DEFAULT_TIMEOUT,
     )
     action = StubbedRequestAction(200, {"fake": "data"})
+    engine = StubbedEngine()
     engine.expect_request(expected_request, action)
     requester = CruxApiRequester("API_KEY", engine)
     result = main("https://example.com", requester)
