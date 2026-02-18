1c731b6c5093a09f33c7a96904e3967d1c1c62ee
Vanessa Prema <vprema@mozilla.com>
2026-02-18 08:11:18 -0800

    MPP-4568 - fix(relay-landing): update free masks string to mention data breaches and remove inbox

--- frontend/src/components/landing/HighlightedFeatures.tsx
+++ frontend/src/components/landing/HighlightedFeatures.tsx
@@ -46,7 +46,7 @@ export const HighlightedFeatures = () => {
             </h3>
             <p className={styles["highlighted-feature-body"]}>
               {l10n.getString(
-                `highlighted-features-section-${props.name}-body`,
+                `highlighted-features-section-${props.name}-body-1`,
                 variables,
               )}
             </p>
