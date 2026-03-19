27ebae8c8929da7abcc139336d8373a70b3436fc
groovecoder <lcrouch@mozilla.com>
2026-03-19 15:20:02 -0500

    chore(e2e-tests): remove visual regression tests completely

--- e2e-tests/specs/relay-general-functionality.spec.ts
+++ e2e-tests/specs/relay-general-functionality.spec.ts
@@ -1,7 +1,6 @@
 import test, { expect } from "../fixtures/basePages";
 import {
   checkAuthState,
-  defaultScreenshotOpts,
   ENV_URLS,
   fetchMaxNumFreeAliases,
   MAX_NUM_FREE_ALIASES,
@@ -40,77 +39,6 @@ test.describe("Free - General Functionalities, Desktop", () => {
     );
     expect(await dashboardPage.maxMaskBannerText.innerText()).toContain(
       "mask limit",
-    );
-  });
-});
-
-test.describe("Free - General Functionalities, Desktop - Visual Regression", () => {
-  test.skip(
-    ({ browserName }) => browserName !== "firefox",
-    "firefox only image comparisons!",
-  );
-
-  test.beforeEach(async ({ dashboardPage, page }) => {
-    await dashboardPage.open();
-    await checkAuthState(page);
-    await dashboardPage.skipOnboarding();
-    await dashboardPage.maybeDeleteMasks();
-  });
-
-  test("Verify that the Header is displayed correctly for a Free user that is logged in, C1812639", async ({
-    dashboardPage,
-  }) => {
-    await expect(dashboardPage.header).toHaveScreenshot(
-      `${process.env.E2E_TEST_ENV}-dashboardHeader.png`,
-      { ...defaultScreenshotOpts },
-    );
-  });
-
-  test("Verify that the extension upgrade banner is shown, C1812641", async ({
-    dashboardPage,
-  }) => {
-    await dashboardPage.relayExtensionBanner.scrollIntoViewIfNeeded();
-    await expect(dashboardPage.relayExtensionBanner).toHaveScreenshot(
-      `${process.env.E2E_TEST_ENV}-relayExtensionBanner.png`,
-      defaultScreenshotOpts,
-    );
-  });
-
-  test("Verify that opened mask cards are displayed correctly to a Free user, C1553070", async ({
-    dashboardPage,
-  }) => {
-    await expect(async () => {
-      await dashboardPage.generateMask(1);
-      expect((await dashboardPage.maskCards.count()) === 1);
-    }).toPass();
-
-    await expect(dashboardPage.maskCards).toHaveScreenshot(
-      `${process.env.E2E_TEST_ENV}-maskCard.png`,
-      {
-        ...defaultScreenshotOpts,
-        mask: [
-          dashboardPage.maskCardGeneratedEmail,
-          dashboardPage.maskCardBottomMeta,
-        ],
-      },
-    );
-  });
-
-  test("Check that the user can delete an mask, and is prompted to confirm before they delete, C1553071", async ({
-    dashboardPage,
-  }) => {
-    await expect(async () => {
-      await dashboardPage.generateMask(1);
-      expect((await dashboardPage.maskCards.count()) === 1);
-      await dashboardPage.maskCardDeleteButton.click();
-    }).toPass();
-
-    await expect(dashboardPage.maskCardDeleteDialogModal).toHaveScreenshot(
-      `${process.env.E2E_TEST_ENV}-maskCardDeleteDialogModal.png`,
-      {
-        ...defaultScreenshotOpts,
-        mask: [dashboardPage.maskCardDeleteDialogModalGeneratedEmail],
-      },
     );
   });
 });
